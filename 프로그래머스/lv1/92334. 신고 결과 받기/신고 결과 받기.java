e9f5a8dc2ed14c5b71f4412f33c47d7f3936f888
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-24 13:34:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.16 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,27 +3,28 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
-        Map<String, Integer> idMap = new HashMap<>(); //각각 번호 부여
-        Map<String, HashSet<String>> map = new HashMap<>(); //신고받은자:신고자
+        Map<String, Integer> idMap = new HashMap<>();
+        Map<String, HashSet<String>> map = new HashMap<>();
         
         for (int i = 0; i<id_list.length; i++) {
             idMap.put(id_list[i], i);
-            map.put(id_list[i], new HashSet<>()); //id별 set만들어서 누가 나 신고했는지 set으로 만듬
+            map.put(id_list[i], new HashSet<>());
         }
         
-        for (String s : report) {
-            String[] sarr = s.split(" ");
-            map.get(sarr[1]).add(sarr[0]); //id별 신고한사람들 set에 넣음
+        for (String str : report) {
+            String[] sarr = str.split(" ");
+            map.get(sarr[1]).add(sarr[0]);
         }
         
         for (int i = 0; i<id_list.length; i++) {
             HashSet<String> set = map.get(id_list[i]);
             if (set.size() >= k) {
-                for (String str : set) {
-                    answer[idMap.get(str)]++;
+                for (String s : set) {
+                    answer[idMap.get(s)]++;
                 }
             }
         }
+        
         
         
         return answer;
