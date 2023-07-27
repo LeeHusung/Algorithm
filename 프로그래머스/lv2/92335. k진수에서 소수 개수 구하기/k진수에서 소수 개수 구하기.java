1fcc4ca622c4c98a5f2961c66110dc97e41aa330
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-27 00:55:23 -0400

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 1.78 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,58 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = -1;
+        
+        String convert = Integer.toString(n, k);
+        int count = 0; //소수 개수
+        String sosu = ""; //소수인지 확인해야함  
+        
+        //반복 돌리다가 0나오기 전까지의 숫자들더해서 소수면 ++;
+        for (int i = 0; i<convert.length(); i++) {
+            
+            if (convert.charAt(i) != '0') {
+                sosu += Character.toString(convert.charAt(i));   
+            } else {
+                if (!sosu.isEmpty()) {
+                int sosuCount = 0;
+                long a = Long.parseLong(sosu);
+                long sqrtA = (long) Math.sqrt(a); // a의 제곱근 계산
+                    
+                    for (int j = 1; j <= sqrtA; j++) {
+                        if (a % j == 0) {
+                            sosuCount++;
+                            if (j * j != a) {
+                                sosuCount++; // 제곱근 이외의 약수도 존재
+                            }
+                        }
+                    }
+                if (sosuCount == 2) {
+                    count++;
+                }
+                sosu = ""; // 소수 확인 후 변수 초기화
+                }
+            }
+        }
+        if (!sosu.isEmpty()) { // 마지막0 이후 마지막 남아있는 소수도 count
+                long a = Long.parseLong(sosu);
+                int sosuCount = 0;
+                 long sqrtA = (long) Math.sqrt(a); // a의 제곱근 계산
+                    
+                    for (int j = 1; j <= sqrtA; j++) {
+                        if (a % j == 0) {
+                            sosuCount++;
+                            if (j * j != a) {
+                                sosuCount++; // 제곱근 이외의 약수도 존재
+                            }
+                        }
+                    }
+                if (sosuCount == 2) {
+                    count++;
+                }
+        }
+                System.out.println(count);
+      
+        
+        return count;
+    }
+}
