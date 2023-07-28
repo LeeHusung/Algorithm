8f7a1fdfdd4af733b42b0efee778e628c294011a
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-28 14:56:10 +0900

    ［1차］ 캐시.java

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,32 +2,33 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
         String[] city = new String[cities.length];
-        
-        for (int i = 0; i<cities.length; i++) {
-            city[i] = cities[i].toLowerCase();
-        }
         
         List<String> list = new LinkedList<>();
         
         if (cacheSize == 0) {
             return cities.length * 5;
-        }   for (int i = 0; i < city.length; i++) {
-            if (list.contains(city[i])) {
-                // Cache hit: 해당 도시 이름을 삭제하고 가장 최근으로 추가
+        }
+        
+        for (int i = 0; i<cities.length; i++) {
+            city[i] = cities[i].toLowerCase();
+        }
+        
+        for (int i = 0; i<city.length; i++) {
+            if (list.contains(city[i])){
+                answer += 1;
                 list.remove(city[i]);
                 list.add(city[i]);
-                answer += 1;
             } else {
-                // Cache miss: 가장 오래된 항목 제거하고 새로운 항목 추가
-                if (list.size() >= cacheSize) {
+                if (list.size() >= cacheSize) { //리스트가 캐시크기보다 커지면 리스트 0번 인덱스 제거
                     list.remove(0);
                 }
                 list.add(city[i]);
                 answer += 5;
             }
         }
+        
+        
         
         return answer;
     }
