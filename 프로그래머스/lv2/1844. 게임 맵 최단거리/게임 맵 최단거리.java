cc68f8e32fb84981f5708664811742473b3c2b89
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-29 08:36:23 -0400

    [level 2] Title: 게임 맵 최단거리, Time: 3.10 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,8 +1,8 @@
 import java.util.*;
 class Solution {
     
-    int[] dx = {1, 0, -1, 0};
-    int[] dy = {0, 1, 0, -1};
+    int[] dx = {0, 1, 0, -1}; //이동 방향 나타내는 배열. 상하좌우
+    int[] dy = {1, 0, -1, 0};
     
     public int solution(int[][] maps) {
         int answer = 0;
@@ -10,6 +10,7 @@ class Solution {
         int[][] visited = new int[maps.length][maps[0].length];
         
         bfs(maps, visited);
+        
         answer = visited[maps.length-1][maps[0].length-1];
         
         if (answer == 0) {
@@ -19,12 +20,14 @@ class Solution {
         return answer;
     }
     
-    public void bfs(int[][] maps, int[][] visited) {
+    
+    private void bfs(int[][] maps, int[][] visited) {
+        
         int x = 0;
         int y = 0;
-        visited[x][y] = 1;
-        Queue<int[]> q = new LinkedList<>();
+        visited[x][y] = 1; //처음 출발
         
+        Queue<int[]> q = new LinkedList<>(); //먼저 들어간게 먼저 나올 수 있게
         q.add(new int[]{x, y});
         
         while (!q.isEmpty()) {
@@ -36,16 +39,15 @@ class Solution {
                 int bx = cx + dx[i];
                 int by = cy + dy[i];
                 
-                if (bx < 0 || bx > maps.length-1 || by < 0 || by > maps[0].length-1) {
-                    continue;
-                }
-                
+                if (bx < 0 || bx > maps.length-1 || by < 0 || by > maps[0].length-1) continue;
                 if (visited[bx][by] == 0 && maps[bx][by] == 1) {
                     visited[bx][by] = visited[cx][cy] + 1;
                     q.add(new int[]{bx, by});
-                }
+                } 
             }
         }
+        
+        
         
         
     }
