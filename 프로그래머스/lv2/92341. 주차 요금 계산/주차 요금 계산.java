68bd4799e4dab5cd2435303dc219d21e37aa92c9
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-29 14:09:45 +0900

    주차 요금 계산.java

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,47 +2,47 @@ import java.util.*;
 class Solution {
     public double[] solution(int[] fees, String[] records) {
 
-        Map<String, Long> map = new HashMap<>();
-        Map<String, Long> car_time = new TreeMap<>(); //차량번호:주차시간 * TreeMap으로해야 정렬됨
+        Map<String, Long> map = new HashMap<>(); //차번호:입차시간
+        Map<String, Long> car_time = new TreeMap<>(); //차번호:주차시간, 정렬위해 TreeMap사용
         
         for (int i = 0; i<records.length; i++) {
             String[] rSplit = records[i].split(" ");
-        
             String[] time = rSplit[0].split(":");
-            long hour = Integer.parseInt(time[0]) * 60;
-            long minute = Integer.parseInt(time[1]);
-            long sumTime = hour + minute;
-        
-                if (rSplit[2].equals("IN")) {
-                    map.put(rSplit[1], sumTime);
-                } else { //"OUT"
-                    if (map.containsKey(rSplit[1])) {
-                        long parkTime = sumTime - map.get(rSplit[1]); //주차한 시간
-                        car_time.put(rSplit[1], car_time.getOrDefault(rSplit[1], 0L) + parkTime);
-                        map.remove(rSplit[1]);
-                    }
+            long hour = Long.parseLong(time[0]) * 60;
+            long min = Long.parseLong(time[1]);
+            long sum = hour + min;
+            
+            if (rSplit[2].equals("IN")){
+                map.put(rSplit[1], sum);
+            } else { //OUT
+                if (map.containsKey(rSplit[1])) {
+                    long sumTime = sum - map.get(rSplit[1]);
+                    car_time.put(rSplit[1], car_time.getOrDefault(rSplit[1], 0L) + sumTime);
+                    map.remove(rSplit[1]);
                 }
             }
+        }
         
+        //out이 없는 경우 -> map에만 IN 시간 담겨있음.
         for (String carNum : map.keySet()) {
             long noOut = 1439 - map.get(carNum);
             car_time.put(carNum, car_time.getOrDefault(carNum, 0L) + noOut);
         }
-            System.out.println(car_time.values());
-        List<Double> list = new ArrayList<>();
+        
+        List<Double> list = new ArrayList<>(); //요금 담을 배열
         
         for (String carNum : car_time.keySet()) {
-            double totalTime = car_time.get(carNum);
-            if (totalTime >= fees[0]) {
-                double l = Math.ceil((totalTime - fees[0]) / fees[2]) * fees[3];
-                list.add(l+fees[1]);
+            double realTime = car_time.get(carNum);
+            if (realTime >= fees[0]) {
+                double d = Math.ceil((realTime - fees[0]) / fees[2]) * fees[3];
+                list.add((double)fees[1] + d);
             } else {
-                // double l = Math.ceil((totalTime - fees[0]) / fees[2]) * fees[3];
                 list.add((double)fees[1]);
             }
         }
         
         double[] answer = new double[list.size()];
+        
         for (int i = 0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
