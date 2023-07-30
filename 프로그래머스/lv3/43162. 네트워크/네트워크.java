99b3b403485fdcc45aeeb727a9ea66a6dcf03fda
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-30 12:51:55 +0900

    네트워크.java

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -9,23 +9,22 @@ class Solution {
         }
         
         for (int i = 0; i<n; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, computers);
+            if (!visited[i]) { //아직 방문되지 않았다면
+                dfs(i, visited, computers); 
+                // i번 컴퓨터에 대한 모든 노드들 탐색 후 방문처리 완료
                 answer++;
             }
         }
-        
-        
         return answer;
     }
     
     
     public boolean[] dfs(int i, boolean[] visited, int[][] computers) {
-        visited[i] = true;
+        visited[i] = true; //방문처리
         
         for (int j = 0; j<computers.length; j++) {
-            if (i != j && computers[i][j] == 1 && visited[j] == false) {
-                visited = dfs(j, visited, computers);
+            if (i != j && computers[i][j] == 1 && visited[j] == false) { //다음방문할 노드 j가 false면
+                visited = dfs(j, visited, computers); //노드 j부터 다시 탐색 -> 이어진 모드 노드들 방문처리됨.
             }
         }
         return visited;
