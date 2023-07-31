879d7470bb2451790ed67efbf74ee4bbe66aa79e
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-31 14:04:07 +0900

    튜플.java

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,37 +1,38 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-
-        String[] sSplit = s.split("\\},\\{"); //why?
         
+        String[] sSplit = s.split("\\},\\{");
         sSplit[0] = sSplit[0].replaceAll("[{}]", "");
         sSplit[sSplit.length-1] = sSplit[sSplit.length-1].replaceAll("[{}]", "");
-        System.out.println(sSplit[0]);
         
-        int[] answer = new int[sSplit.length];
-        String[][] sarrByLength = new String[sSplit.length][];
+        // 길이 순 정렬
+        String[][] sortByLength = new String[sSplit.length][];
         
-        //길이순 정렬
         for (int i = 0; i<sSplit.length; i++) {
             String[] sarr = sSplit[i].split(",");
-            sarrByLength[sarr.length-1] = sarr;
+            sortByLength[sarr.length-1] = sarr;
         }
-        
+        // set에 넣고 값 구분
         Set<String> set = new HashSet<>();
-        String[] list = new String[sSplit.length];
+        String[] list = new String[sSplit.length]; //중복x인 문자 넣기
         
-        for (int i = 0; i<sarrByLength.length; i++) {
-            for (int j = 0; j<sarrByLength[i].length; j++) {
-                if (!set.contains(sarrByLength[i][j])) {
-                    list[i] = sarrByLength[i][j];
-                    set.add(sarrByLength[i][j]);
+        for (int i = 0; i<sortByLength.length; i++) {
+            for (int j = 0; j<sortByLength[i].length; j++) {
+                if (!set.contains(sortByLength[i][j])) {
+                    list[i] = sortByLength[i][j];
+                    set.add(sortByLength[i][j]);
                 }
             }
         }
         
+        int[] answer = new int[sSplit.length];
+        
         for (int i = 0; i<list.length; i++) {
             answer[i] = Integer.parseInt(list[i]);
         }
+        
+        
         
         return answer;
     }
