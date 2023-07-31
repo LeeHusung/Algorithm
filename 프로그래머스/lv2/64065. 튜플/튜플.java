87c9f2671c95e28050f38bc27906a45fed3106d9
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-07-31 00:50:39 -0400

    [level 2] Title: 튜플, Time: 0.40 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,43 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        
-       String[] sSplit = s.split("\\},\\{");     
-        // ["{{2", "2,1", "2,1,3", "2,1,3,4}}"]
 
-        // 배열의 가장 앞 문자열, 배열의 가장 뒷 문자열에서 '{' , '}' 문자 제거
-        sSplit[0] = sSplit[0].replaceAll("[{}]","");
-        sSplit[sSplit.length-1] = sSplit[sSplit.length-1].replaceAll("[{}]","");
-        // ["2", "2,1", "2,1,3", "2,1,3,4"]
+        String[] sSplit = s.split("\\},\\{"); //why?
         
-        // System.out.println(sSplit[0]);
-        // System.out.println(sSplit[sSplit.length-1]);
+        sSplit[0] = sSplit[0].replaceAll("[{}]", "");
+        sSplit[sSplit.length-1] = sSplit[sSplit.length-1].replaceAll("[{}]", "");
+        System.out.println(sSplit[0]);
+        
         int[] answer = new int[sSplit.length];
+        String[][] sarrByLength = new String[sSplit.length][];
         
-        
-         String[][] sarr = new String[sSplit.length][];
-
-        for (int i = 0 ; i < sSplit.length ; i++) {
-            String[] ss = sSplit[i].split(",");
-            sarr[ss.length-1] = ss;
-            //System.out.println(Arrays.toString(sortedByLength[ss.length-1]));
+        //길이순 정렬
+        for (int i = 0; i<sSplit.length; i++) {
+            String[] sarr = sSplit[i].split(",");
+            sarrByLength[sarr.length-1] = sarr;
         }
-        Set<String> set = new HashSet<>();
-        String[] list = new String[sSplit.length]; //정답배열
         
-        for (int i = 0; i<sarr.length; i++) {
-            for (int j = 0; j<sarr[i].length; j++) {
-                if (!set.contains(sarr[i][j])) {
-                    list[i] = sarr[i][j];
-                    set.add(sarr[i][j]);
+        Set<String> set = new HashSet<>();
+        String[] list = new String[sSplit.length];
+        
+        for (int i = 0; i<sarrByLength.length; i++) {
+            for (int j = 0; j<sarrByLength[i].length; j++) {
+                if (!set.contains(sarrByLength[i][j])) {
+                    list[i] = sarrByLength[i][j];
+                    set.add(sarrByLength[i][j]);
                 }
             }
-        }        
+        }
         
         for (int i = 0; i<list.length; i++) {
             answer[i] = Integer.parseInt(list[i]);
         }
-        
         
         return answer;
     }
