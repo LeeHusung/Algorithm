c4e2a532923f0b037cb4d81d78ff81f8921d051a
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-08-13 13:31:13 +0900

    스킬트리.java

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,28 +1,29 @@
-import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
         for (String s : skill_trees) {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < s.length(); i++) {
-                String temp = String.valueOf(s.charAt(i));
-                if (skill.contains(temp)) {
-                    sb.append(temp);
+            for (int i = 0; i<s.length(); i++) {
+                String str = String.valueOf(s.charAt(i));
+                if (skill.contains(str)) {
+                    sb.append(str);
                 }
             }
             
-            boolean flag = false;
-            for (int i = 0; i < sb.length(); i++) {
+            boolean flag = true;
+            for (int i = 0; i<sb.length(); i++) {
                 if (skill.charAt(i) != sb.toString().charAt(i)) {
-                    flag = true;
+                    flag = false;
                     break;
                 }
             }
-            if (!flag) {
+            if (flag) {
                 answer += 1;
             }
         }
+       
         return answer;
     }
 }
