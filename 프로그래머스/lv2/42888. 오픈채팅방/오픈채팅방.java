d00757f6b0994bdfae87012544d4109cf6fe3fe0
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-08-14 23:40:35 -0400

    [level 2] Title: 오픈채팅방, Time: 119.26 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,10 +1,11 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
-        String sb = "";
-        
+
         Map<String, String> map = new HashMap<>();
         List<String> list = new ArrayList<>();
+        
+        
         for (String records : record) {
             String[] recordSplit = records.split(" ");
             if (recordSplit[0].equals("Enter") || recordSplit[0].equals("Change")) {
@@ -20,13 +21,8 @@ class Solution {
             if (recordSplit[0].equals("Leave")) {
                 list.add(map.get(recordSplit[1]) + "님이 나갔습니다.");
             }
-            
-            
         }
         String[] answer = list.toArray(new String[0]);
-        
-        
-        
         
         return answer;
     }
