bdb71ddcb1c2989de952fe861cf12ae6df436684
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-08-17 04:59:33 -0400

    [level 2] Title: [3차] 압축, Time: 0.41 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,43 +1,41 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String msg) {
-        List<Integer> answerList = new ArrayList<>();
-        Map<String, Integer> dictionary = new HashMap<>();
         
-        // 초기 사전 설정 (단일 문자를 코드로 매핑)
-        for (int i = 0; i < 26; i++) {
-            dictionary.put(String.valueOf((char) ('A' + i)), i + 1);
+        List<Integer> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i<26; i++) {
+            map.put(String.valueOf((char) ('A' + i)), i+1);
         }
         
-        int nextIndex = 27; // 다음 사용 가능한 색인 번호
-        
+        int next = 27;
         int i = 0;
         while (i < msg.length()) {
-            int j = i + 1;
-            // 사전에서 찾을 수 있는 가장 긴 문자열을 찾음
-            while (j <= msg.length() && dictionary.containsKey(msg.substring(i, j))) {
-                j++;
+            int j = i+1;
+            while (j <= msg.length() && map.containsKey(msg.substring(i, j))) {
+                j++; //map이 가지고 있는 최대 길이 단어 구하기
             }
-            j--; // j를 사전에서 찾은 문자열의 끝 인덱스로 조정
+            j--; //list에 넣을 인덱스 구하기
             
-            // 사전에서 찾은 문자열에 해당하는 색인 번호를 출력 리스트에 추가
-            answerList.add(dictionary.get(msg.substring(i, j)));
+            list.add(map.get(msg.substring(i, j)));
             
-            // 다음에 올 문자열을 사전에 추가
+            //msg의 길이 안넘는다면 뒤에 한 문자 추가해서 map에 더하기. 넘으면 반복 다 돈거임.
             if (j < msg.length()) {
-                dictionary.put(msg.substring(i, j + 1), nextIndex);
-                nextIndex++;
+                map.put(msg.substring(i, j+1), next);
+                next++;
             }
             
-            i = j; // 다음 처리할 위치로 인덱스 업데이트 (j만큼 이동)
+            //list에 값 넣은 다음 단어부터 반복
+            i = j;
         }
         
-        // 리스트를 배열로 변환
-        int[] answer = new int[answerList.size()];
-        for (int k = 0; k < answerList.size(); k++) {
-            answer[k] = answerList.get(k);
+        int[] answer = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            answer[k] = list.get(k);
         }
+        
+        
         
         return answer;
     }
