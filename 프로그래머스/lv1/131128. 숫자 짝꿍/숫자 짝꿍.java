dff387d458e9ee4d299f3a720552ab860d045006
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-08-18 14:10:11 +0900

    숫자 짝꿍.java

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,39 +1,47 @@
-import java.util.Arrays;
-
 class Solution {
     public String solution(String X, String Y) {
-        int[] xCounts = new int[10];
-        int[] yCounts = new int[10];
+        // 숫자의 등장 횟수를 저장할 배열
+        int[] xCount = new int[10];
+        int[] yCount = new int[10];
 
-        for (char digit : X.toCharArray()) {
-            xCounts[digit - '0']++;
+        // 숫자 X의 등장 횟수 계산
+        for (char c : X.toCharArray()) {
+            xCount[c - '0']++;
         }
 
-        for (char digit : Y.toCharArray()) {
-            yCounts[digit - '0']++;
+        // 숫자 Y의 등장 횟수 계산
+        for (char c : Y.toCharArray()) {
+            yCount[c - '0']++;
         }
 
+        // 결과 문자열을 구성할 StringBuilder 생성
         StringBuilder answer = new StringBuilder();
 
+        // 공통으로 나타나는 숫자 중에서 가장 큰 숫자를 찾아 결과 문자열에 추가
         for (int i = 9; i >= 0; i--) {
-            int commonCount = Math.min(xCounts[i], yCounts[i]);
+            int commonCount = Math.min(xCount[i], yCount[i]);
             for (int j = 0; j < commonCount; j++) {
                 answer.append(i);
             }
         }
 
+        // 결과 문자열에 아무런 숫자가 추가되지 않았다면 공통으로 나타나는 숫자가 없음
         if (answer.length() == 0) {
             return "-1";
         }
 
+        // 결과 문자열의 첫 번째 숫자가 0이라면, 0으로만 구성되어 있는 경우
         if (answer.charAt(0) == '0') {
             return "0";
         }
 
+        // 결과 문자열 반환
         return answer.toString();
     }
 }
 
+
+        
 
         
 //         StringBuilder sb = new StringBuilder();
