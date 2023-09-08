dc9f4f45b974bae6d3e588d9bd6400d3b59204d5
이후성 <128116509+LeeHusung@users.noreply.github.com>
2023-09-07 21:42:21 -0400

    [level 2] Title: 방문 길이, Time: 0.08 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,109 @@
+import java.util.*;
+
+class Solution {
+    private static int[] dx = {1, 0, -1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    private static boolean[][][][] visited = new boolean[11][11][11][11];
+    
+    public int solution(String dirs) {
+        int answer = 0;
+        
+        int index = 0;
+        int x = 5; int y = 5;
+        for (char ch : dirs.toCharArray()) {
+            if (ch == 'L') {
+                index = 2;
+            } else if (ch == 'R') {
+                index = 0;
+            } else if (ch == 'U') {
+                index = 3;
+            } else if (ch == 'D') {
+                index = 1;
+            }
+            int nx = x + dx[index];
+            int ny = y + dy[index];
+            
+            if (nx < 0 || nx >= 11 || ny < 0 || ny >= 11) {
+                continue;
+            } 
+            if (!visited[nx][ny][x][y] == true && !visited[x][y][nx][ny] == true) {
+                visited[nx][ny][x][y] = true;
+                visited[x][y][nx][ny] = true;
+                answer++;
+            }
+            
+            x = nx;
+            y = ny;
+        }
+
+        return answer;
+    }
+}
+        
+//         Set<Node> set = new HashSet<>();
+        
+//         int startX = 0;
+//         int startY = 0;
+//         Node node = new Node(startX, startY);
+        
+//         for (int i = 0; i<dirs.length(); i++) {
+//             if (dirs.charAt(i) == 'L') {
+//                 Node newNode = new Node(startX--, startY);
+//                 if (newNode.getX() < -5) {
+//                     startX = -5;
+//                 }
+//                 if (!set.contains(newNode)) {
+//                     set.add(newNode);
+//                 }
+//             } else if (dirs.charAt(i) == 'R') {
+//                 Node newNode = new Node(startX++, startY);
+//                 if (newNode.getX() > 5) {
+//                     startX = 5;
+//                 }
+//                 if (!set.contains(newNode)) {
+//                     set.add(newNode);
+//                 }
+//             } else if (dirs.charAt(i) == 'U') {
+//                 Node newNode = new Node(startX, startY++);
+//                 if (newNode.getY() > 5) {
+//                     startY = 5;
+//                 }
+//                 if (!set.contains(newNode)) {
+//                     set.add(newNode);
+//                 }
+//             } else if (dirs.charAt(i) == 'D') {
+//                 Node newNode = new Node(startX, startY--);
+//                 if (newNode.getY() < -5) {
+//                     startY = -5;
+//                 }
+//                 if (!set.contains(newNode)) {
+//                     set.add(newNode);
+//                 }
+//             }
+//         }
+//         Iterator iter = set.iterator();
+//         while (iter.hasNext()) {
+//             System.out.println(iter.next());
+//         }
+        
+        
+        
+//         return set.size();
+//     }
+    
+//     static class Node {
+//         private int x;
+//         private int y;
+        
+//         Node (int x, int y) {
+//             this.x = x;
+//             this.y = y;
+//         }
+//         public int getX() {
+//             return x;
+//         }
+//         public int getY() {
+//             return y;
+//         }
+//     }
+// }
