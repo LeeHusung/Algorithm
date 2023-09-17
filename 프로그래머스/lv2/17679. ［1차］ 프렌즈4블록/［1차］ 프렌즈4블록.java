8183c518c8a9c944ee003acb522359117a861889
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-09-17 03:57:25 -0400

    [level 2] Title: [1차] 프렌즈4블록, Time: 4.14 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -27,55 +27,53 @@ class Solution {
     
   public int solution(int m, int n, String[] board) {
       Character[][] ch = new Character[m][n];
-      for (int i = 0; i < m; i++) 
-          for (int j = 0; j < n; j++) 
-              ch[i][j] = board[i].charAt(j);
-
-      boolean isUpdated = true;
-      int countTotal=0;
-
-      while(isUpdated){
-          isUpdated=false;
-
-          Set<Node> set=new HashSet<>();
-          for(int i=1;i<m;i++){
-              for(int j=1;j<n;j++){
-                  char block=ch[i][j];
-                  if(block!=' '&&block==ch[i-1][j]&&block==ch[i][j-1]&&block==ch[i-1][j-1]){
-                      set.add(new Node(i,j));
-                      set.add(new Node(i-1,j));
-                      set.add(new Node(i,j-1));
-                      set.add(new Node(i-1,j-1));
+     
+      
+      for (int i = 0; i<m; i++) {
+          for (int j = 0; j<n; j++) {
+             ch[i][j] = board[i].charAt(j);
+          }
+      }
+      boolean isTrue = true;
+      int count = 0;
+      while (isTrue) {
+          isTrue = false;
+      Set<Node> set = new HashSet<>();
+          
+          for (int i = 0; i<ch.length-1; i++) {
+              for (int j = 0; j<ch[i].length-1; j++) {
+                  if (ch[i][j] != ' ' && ch[i][j] == ch[i+1][j] && ch[i][j] == ch[i][j+1] && ch[i][j] == ch[i+1][j+1]) {
+                      set.add(new Node(i, j));
+                      set.add(new Node(i+1, j));
+                      set.add(new Node(i, j+1));
+                      set.add(new Node(i+1, j+1));
                   }
               }
           }
-
-          // remove blocks and count them
-          for(Node node:set){
-              ch[node.x][node.y]=' ';
-              isUpdated=true;
+          
+          count += set.size();
+          
+          for (Node node : set) {
+              ch[node.x][node.y] = ' ';
+              isTrue = true;
           }
-
-         countTotal+=set.size();
-
-         // shift blocks down
-         for(int j=0;j<n;j++){
-             Queue<Character> queue = new LinkedList<>();
-             for(int i=m-1;i>=0;i--){
-                 if(ch[i][j]!=' ') {
-                     queue.offer(ch[i][j]);
-                     ch[i][j]=' ';
-                 }
-             }
-
-             int index=m-1;
-             while(!queue.isEmpty()){
-                 ch[index--][j]=queue.poll(); //큐에서 값 꺼내서 ch 2차원배열 다시 초기화.
-             }
-         }
-     }
-
-     return countTotal;
-
+          
+          //내리기
+          for (int j = 0; j<n; j++) {
+              Queue<Character> q = new LinkedList<>();
+              for (int i = m-1; i >= 0; i--) {
+                  if (ch[i][j] != ' ') {
+                      q.add(ch[i][j]);
+                      ch[i][j] = ' ';
+                  }
+              }
+              int idx = m-1;
+              while (!q.isEmpty()) {
+                  ch[idx--][j] = q.poll();
+              }
+          }
+      }
+      
+      return count;
    }  
 }
