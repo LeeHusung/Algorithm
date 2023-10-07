cbe926fdf061d05fb0206abf6499268f21b5bcb7
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-10-07 03:33:58 -0400

    [Gold IV] Title: 수도배관공사, Time: 352 ms, Memory: 20616 KB -BaekjoonHub

---
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int D = sc.nextInt();
+        int P = sc.nextInt();
+        int[] dp = new int[D + 1];
+        for (int i = 0; i < P; i++) {
+            int L = sc.nextInt();
+            int C = sc.nextInt();
+            for (int j = D; j > L; j--) {
+                if (dp[j - L] == 0) continue;
+                dp[j] = Math.max(dp[j], Math.min(dp[j - L], C));
+            }
+            dp[L] = Math.max(dp[L], C);
+        }
+        System.out.println(dp[D]);
+    }
+}
