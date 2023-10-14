35492a7f256cff6e65a02bfd22553fe9b69580b4
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-10-14 02:11:43 -0400

    [Gold III] Title: 수확, Time: 388 ms, Memory: 38116 KB -BaekjoonHub

--- 백준/Gold/1823. 수확/수확.java
+++ 백준/Gold/1823. 수확/수확.java
@@ -13,21 +13,17 @@ public class Main {
         int[][] dp = new int[n + 1][n + 1];
 
         for (int i = 1; i < n + 1; i++) dp[i][i] = arr[i];
-//
-//        int[] s = new int[n + 1];
-//        int c = 0;
-//        for (int i = 1; i < s.length; i++) {
-//            c += arr[i];
-//            s[i] = c;
-//        }
+
+        int[] s = new int[n + 1];
+        int c = 0;
+        for (int i = 1; i < s.length; i++) {
+            c += arr[i];
+            s[i] = c;
+        }
 
         for (int i = 1; i < n; i++) {
             for (int j = 1; j <= n-i; j++) {
-                int cnt = 0;
-                for (int k = j; k <= j + i; k++) {
-                    cnt += arr[k];
-                }
-                dp[j][j+i] = Math.max(dp[j][j+i-1], dp[j+1][j+i]) + cnt;
+                dp[j][j+i] = Math.max(dp[j][j+i-1], dp[j+1][j+i]) + s[j+i] - s[j-1];
             }
         }
 
