090a08ce053333cdcdc1695744960be7d5de4257
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-11-19 15:07:05 +0900

    [Silver III] Title: N과 M (4), Time: 780 ms, Memory: 26972 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static LinkedList<Integer> list = new LinkedList<>();
+
+    public static void D(int L) {
+        if (L == m) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i) + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (!list.isEmpty() && arr[i] < list.peekLast()) continue;
+                list.add(arr[i]);
+                D(L + 1);
+                list.removeLast();
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        D(0);
+    }
+}
