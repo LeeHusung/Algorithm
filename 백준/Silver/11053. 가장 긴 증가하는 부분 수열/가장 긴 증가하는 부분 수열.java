fb72ee861e3e0c7dea879bbf06fccf7f8da74f11
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-11-28 13:08:19 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,28 +1,31 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        int[] arr = new int[n + 1];
+        String[] s = br.readLine().split(" ");
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(s[i-1]);
         }
-        Arrays.fill(dp, 1);
-        for (int i = 1; i < dp.length; i++) {
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            int cur = arr[i];
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
-                }
+                if (cur > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
         int answer = 0;
-        for (int i = 0; i < dp.length; i++) {
-            answer = Math.max(answer, dp[i]);
+        for (int x : dp) {
+            answer = Math.max(answer, x);
         }
+
         System.out.println(answer);
     }
 }
