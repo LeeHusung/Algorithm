3a04ecc5fe2bfb9cff9455b96889a24c6c1f8f0f
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-11-30 13:12:20 +0900

    [Gold IV] Title: 테트로미노, Time: 452 ms, Memory: 39600 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -22,101 +22,89 @@ public class Main {
             }
         }
 
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < 23; i++) {
-            map.put((char) ('a' + i), 0);
-        }
-
+        int answer = 0;
         for (int y = 0; y < N; y++) {
             for (int z = 0; z < M; z++) {
                 if (z + 3 < M) {
-                    map.put('a', arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y][z + 3]);
+                    int temp =  arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y][z + 3];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 3 < N) {
-                    map.put('b', arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y + 3][z]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y + 3][z];
+                    if (temp > answer) answer = temp;
                 }
                 if (z + 1 < M && y + 1 < N) {
-                    map.put('c', arr[y][z] + arr[y + 1][z] + arr[y][z + 1] + arr[y + 1][z + 1]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y][z + 1] + arr[y + 1][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 2 < N && z + 1 < M) {
-                    map.put('d', arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y + 2][z + 1]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y + 2][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y - 2 >= 0 && z + 1 < M) {
-                    map.put('e', arr[y][z] + arr[y][z + 1] + arr[y - 1][z + 1] + arr[y - 2][z + 1]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y - 1][z + 1] + arr[y - 2][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 2 < N && z + 1 < M) {
-                    map.put('f', arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y][z + 1]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 2 < N && z + 1 < M) {
-                    map.put('g', arr[y][z] + arr[y][z + 1] + arr[y + 1][z + 1] + arr[y + 2][z + 1]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y + 1][z + 1] + arr[y + 2][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 1 < N && z + 2 < M) {
-                    map.put('h', arr[y][z] + arr[y + 1][z] + arr[y][z + 1] + arr[y][z + 2]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y][z + 1] + arr[y][z + 2];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 1 < N && z + 2 < M) {
-                    map.put('i', arr[y][z] + arr[y + 1][z] + arr[y + 1][z + 1] + arr[y + 1][z + 2]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y + 1][z + 1] + arr[y + 1][z + 2];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 1 < N && z + 2 < M) {
-                    map.put('j', arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y + 1][z + 2]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y + 1][z + 2];
+                    if (temp > answer) answer = temp;
                 }
                 if (y - 1 >= 0 && z + 2 < M) {
-                    map.put('k', arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y - 1][z + 2]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y - 1][z + 2];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 2 < N && z + 1 < M) {
-                    map.put('l', arr[y][z] + arr[y + 1][z] + arr[y + 1][z + 1] + arr[y + 2][z + 1]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y + 1][z + 1] + arr[y + 2][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y - 1 >= 0 && y + 1 < N && z + 1 < M) {
-                    map.put('m', arr[y][z] + arr[y][z + 1] + arr[y + 1][z] + arr[y - 1][z + 1]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y + 1][z] + arr[y - 1][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y - 1 >= 0 && z + 2 < M) {
-                    map.put('n', arr[y][z] + arr[y][z + 1] + arr[y - 1][z + 1] + arr[y - 1][z + 2]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y - 1][z + 1] + arr[y - 1][z + 2];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 1 < N && z + 2 < M) {
-                    map.put('o', arr[y][z] + arr[y][z + 1] + arr[y + 1][z + 1] + arr[y + 1][z + 2]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y + 1][z + 1] + arr[y + 1][z + 2];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 1 < N && z + 2 < M) {
-                    map.put('p', arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y + 1][z + 1]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y + 1][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 1 < N && y - 1 >= 0 && z + 1 < M) {
-                    map.put('q', arr[y][z] + arr[y][z + 1] + arr[y - 1][z + 1] + arr[y + 1][z + 1]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y - 1][z + 1] + arr[y + 1][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y + 2 < N && z + 1 < M) {
-                    map.put('r', arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y + 1][z + 1]);
+                    int temp = arr[y][z] + arr[y + 1][z] + arr[y + 2][z] + arr[y + 1][z + 1];
+                    if (temp > answer) answer = temp;
                 }
                 if (y - 1 >= 0 && z + 2 < M) {
-                    map.put('s', arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y - 1][z + 1]);
+                    int temp = arr[y][z] + arr[y][z + 1] + arr[y][z + 2] + arr[y - 1][z + 1];
+                    if (temp > answer) answer = temp;
                 }
-//                if (y + 1 < N && z + 2 < M) {
-//                    map.put('t', arr[y][z] + arr[y][z + 1] + arr[y + 1][z + 1] + arr[y][z + 2]);
-//                }
-//                if (y + 2 < N && z + 1 < M) {
-//                    map.put('u', arr[y][z] + arr[y + 1][z] + arr[y + 1][z + 1] + arr[y + 2][z]);
-//                }
-//                if (y + 1 < N && z + 2 < M) {
-//                    map.put('v', arr[y][z] + arr[y][z + 1] + arr[y + 1][z + 1] + arr[y + 1][z + 2]);
-//                }
-//                if (y + 1 < N && y - 1 >= 0 && z + 1 < M) {
-//                    map.put('w', arr[y][z] + arr[y + 1][z] + arr[y - 1][z + 1] + arr[y][z + 1]);
-//                }
 
-                int max = 0;
-                for (char ch : map.keySet()) {
-                    max = Math.max(max, map.get(ch));
-                }
-                tmp[y][z] = max;
             }
         }
-
-        int answer = 0;
-        for (int t = 0; t < N; t++) {
-            for (int u = 0; u < M; u++) {
-                answer = Math.max(answer, tmp[t][u]);
-            }
-        }
-
         System.out.println(answer);
-
-
 
     }
 }
