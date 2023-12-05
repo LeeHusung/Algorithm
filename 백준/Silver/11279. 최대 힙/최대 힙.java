304af4bf2d34cfdb8031bc08c0a15d709411ca8a
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-12-05 18:02:55 +0900

    [Silver II] Title: 최대 힙, Time: 1108 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) pq.offer(x);
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                    continue;
+                }
+                System.out.println(pq.poll());
+            }
+
+
+        }
+
+    }
+}
