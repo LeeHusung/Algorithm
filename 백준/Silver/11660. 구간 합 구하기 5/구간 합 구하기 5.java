ced4033c75369186e3e71b29edda70e5ec3ac209
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-12-07 14:46:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1912 ms, Memory: 154048 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        int[][] arr = new int[n+1][n+1];
+        int[][] dp = new int[n+1][n+1];
+
+        for (int i = 1; i <= n; i++) {
+            String[] s1 = br.readLine().split(" ");
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(s1[j - 1]);
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i][j] - dp[i-1][j-1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] s1 = br.readLine().split(" ");
+            int x1 = Integer.parseInt(s1[0]);
+            int y1 = Integer.parseInt(s1[1]);
+            int x2 = Integer.parseInt(s1[2]);
+            int y2 = Integer.parseInt(s1[3]);
+
+            System.out.println((dp[x2][y2] - dp[x2][y1 - 1] - dp[x1-1][y2] + dp[x1-1][y1-1]));
+        }
+    }
+}
