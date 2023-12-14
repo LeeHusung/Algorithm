f71cf667b82a222c849e2ef35f96ae4c82ab6fbe
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-12-14 20:39:04 +0900

    [Gold V] Title: 리모컨, Time: 988 ms, Memory: 303680 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
@@ -10,12 +11,13 @@ public class Main {
     static LinkedList<Integer> list = new LinkedList<>();
 
     public static void D(int L) {
+        //처음에 list.size == len으로 했다가 틀림. 1의자리부터 모든 경우의 수 탐색해야함.
         if (list.size() >= 1) {
-            StringBuilder str = new StringBuilder();
+            String str = "";
             for (int x : list) {
-                str.append(x);
+                str += x;
             }
-            int parse = Integer.parseInt(str.toString());
+            int parse = Integer.parseInt(str);
             int distance = Math.abs(n - parse);
             answer = Math.min(answer, L + distance);
         }
@@ -27,31 +29,41 @@ public class Main {
             }
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
         answer = Math.abs(n - 100);
-        len = String.valueOf(n).length();
-        
-        boolean[] noWork = new boolean[10];
+        String str2 = String.valueOf(n);
+        len = str2.length();
+        if (m == 0) {
+            nums = new int[10];
+            for (int i = 0; i < 10; i++) {
+                nums[i] = i;
+            }
+
+            answer = Math.min(len, answer);
+//            D(0);
+            System.out.println(answer);
+            return;
+        }
         if (m > 0) {
             String[] s1 = br.readLine().split(" ");
+            List<Integer> noWork = new LinkedList<>();
             for (int i = 0; i < s1.length; i++) {
-                noWork[Integer.parseInt(s1[i])] = true;
+                noWork.add(Integer.parseInt(s1[i]));
             }
-        }
-        
-        nums = new int[10 - m];
-        int idx = 0;
-        for (int i = 0; i < 10; i++) {
-            if (!noWork[i]) {
-                nums[idx++] = i;
+            nums = new int[10 - noWork.size()];
+            int idx = 0;
+            for (int i = 0; i < 10; i++) {
+                if (!noWork.contains(i)) {
+                    nums[idx++] = i;
+                }
             }
+            D(0);
+            System.out.println(answer);
         }
 
-        D(0);
-        System.out.println(answer);
+
     }
 }
