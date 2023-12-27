e383dfdbcd9f1b0aee8245800911f32bb1db4e55
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-12-27 16:34:47 +0900

    [Gold IV] Title: 알파벳, Time: 1024 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
@@ -11,28 +9,22 @@ public class Main {
     static int[] ch;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
-    static Queue<int[]> q = new LinkedList<>();
 
     private static void D(int L, int x, int y) {
-        if (ch[arr[x][y]] == 1) return;
-        while (!q.isEmpty()) {
-            int len = q.size();
+        if (ch[arr[x][y]] == 1) {
             answer = Math.max(L, answer);
-            for (int i = 0; i < len; i++) {
-                int[] p = q.poll();
+            return;
+        }
 
-                ch[arr[p[0]][p[1]]] = 1;
-                for (int j = 0; j < 4; j++) {
-                    int nx = p[0] + dx[j];
-                    int ny = p[1] + dy[j];
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && ch[arr[nx][ny]] == 0) {
-                        q.offer(new int[]{nx, ny});
-                        D(L + 1, nx, ny);
-                        ch[arr[nx][ny]] = 0;
-                    }
-                }
+        ch[arr[x][y]] = 1;
+        for (int j = 0; j < 4; j++) {
+            int nx = x + dx[j];
+            int ny = y + dy[j];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                D(L + 1, nx, ny);
             }
         }
+        ch[arr[x][y]] = 0;
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,18 +32,17 @@ public class Main {
         String[] s = br.readLine().split(" ");
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
+        answer = 1;
         ch = new int[100];
-        arr = new int[n][m];
+        arr = new int[21][21];
         for (int i = 0; i < n; i++) {
             String[] split = br.readLine().split("");
             for (int j = 0; j < m; j++) {
-                arr[i][j] = split[j].charAt(0);
+                arr[i][j] = split[j].charAt(0) - 'A';
             }
         }
-        q.offer(new int[]{0, 0});
-        D(1, 0, 0);
+        D(0, 0, 0);
         System.out.println(answer);
-
 
     }
 }
