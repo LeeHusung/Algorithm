a3ee4625e4ff5028527ad970b53be6c5ccefd901
LHS <128116509+LeeHusung@users.noreply.github.com>
2023-12-29 12:20:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 656 ms, Memory: 61000 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,50 +1,49 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int cost = sc.nextInt();
-            graph.get(x).add(new int[]{y, cost});
-        }
-        int start = sc.nextInt();
-        int end = sc.nextInt();
-
-        int[][] dist = new int[n + 1][n + 1];
-        for (int i = 0; i < n + 1; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == j) dist[i][j] = 0;
-            }
+            String[] s = br.readLine().split(" ");
+            int start = Integer.parseInt(s[0]);
+            int end = Integer.parseInt(s[1]);
+            int cost = Integer.parseInt(s[2]);
+            graph.get(start).add(new int[]{end, cost});
         }
 
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{start, 0});
+        String[] s = br.readLine().split(" ");
+        int startPoint = Integer.parseInt(s[0]);
+        int endPoint = Integer.parseInt(s[1]);
 
-        while (!q.isEmpty()) {
-            int[] p = q.poll();
-            if (p[1] > dist[start][p[0]]) continue;
+        int[] res = new int[n + 1];
+        Arrays.fill(res, Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.offer(new int[]{startPoint, 0});
+        while (!pq.isEmpty()) {
+            int[] p = pq.poll();
+            if (p[1] > res[p[0]]) continue;
             for (int[] x : graph.get(p[0])) {
-                int cost = p[1] + x[1];
-                if (dist[start][x[0]] > cost) {
-                    dist[start][x[0]] = cost;
-                    q.offer(new int[]{x[0], cost});
+                if (res[x[0]] > x[1] + p[1]) {
+                    res[x[0]] = x[1] + p[1];
+                    pq.offer(new int[]{x[0], x[1] + p[1]});
                 }
             }
         }
-        System.out.println(dist[start][end]);
+        System.out.println(res[endPoint]);
 
     }
 }
