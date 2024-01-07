9ecc3344e13578478ddfe6606bbe460891e12e10
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-01-07 12:15:49 +0900

    [Silver II] Title: 주식, Time: 1096 ms, Memory: 370004 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            long[] arr = new long[n];
+            String[] s = br.readLine().split(" ");
+            int idx = n - 1;
+            for (int j = 0; j < n; j++) {
+                arr[j] = Integer.parseInt(s[idx--]);
+            }
+
+            long answer = 0;
+
+            long x = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] > x) {
+                    x = arr[j];
+                } else {
+                    answer += x - arr[j];
+                }
+            }
+
+
+            System.out.println(answer);
+        }
+
+    }
+}
