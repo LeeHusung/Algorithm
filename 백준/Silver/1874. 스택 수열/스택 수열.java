3eafdc88d810d3a11c405f805bea32facd9e1d76
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-01-12 14:35:10 +0900

    [Silver II] Title: 스택 수열, Time: 368 ms, Memory: 25960 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+    //1874
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,25 +14,29 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
-        StringBuilder sb = new StringBuilder();
         Stack<Integer> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         int idx = 0;
-        for (int i = 0; i < n; i++) {
-            st.push(i + 1);
-            sb.append("+");
+        for (int i = 1; i <= n; i++) {
             while (!st.isEmpty() && st.peek() == arr[idx]) {
-                sb.append("-");
-                idx++;
                 st.pop();
+                sb.append("-").append("\n");
+                idx++;
+                continue;
+            }
+            st.push(i);
+            sb.append("+").append("\n");
+
+        }
+        if (!st.isEmpty()) {
+            while (idx < n && arr[idx] == st.peek()) {
+                sb.append("-").append("\n");
+                st.pop();
+                idx++;
             }
         }
-
-        if (st.isEmpty()) {
-            for (int i = 0; i < sb.length(); i++) {
-                System.out.println(sb.charAt(i));
-            }
-        } else System.out.println("NO");
-
+        if (!st.isEmpty()) {
+            System.out.println("NO");
+        } else System.out.println(sb);
     }
 }
