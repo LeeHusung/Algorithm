6f0062281ae5fa318113878cd74226e03a9944a7
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-02-06 19:00:39 +0900

    [Silver I] Title: 안전 영역, Time: 388 ms, Memory: 65816 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, answer;
+    static int[][] arr, ch, clone;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static Queue<int[]> q = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        answer = 1;
+        arr = new int[n][n];
+        clone = new int[n][n];
+        ch = new int[n][n];
+        int max = 0;
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+
+
+        for (int i = 1; i <= max; i++) {
+
+            for (int y = 0; y < n; y++) {
+                clone[y] = arr[y].clone();
+            }
+            ch = new int[n][n];
+
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (clone[j][k] <= i) {
+                        clone[j][k] = -1;
+                    }
+                }
+            }
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (clone[j][k] != -1 && ch[j][k] == 0) {
+                        cnt++;
+                        q.offer(new int[]{j, k});
+                        ch[j][k] = 1;
+                        while (!q.isEmpty()) {
+                            int[] p = q.poll();
+                            for (int l = 0; l < 4; l++) {
+                                int nx = p[0] + dx[l];
+                                int ny = p[1] + dy[l];
+                                if (nx >= 0 && ny >= 0 && nx < n && ny < n && ch[nx][ny] == 0 && clone[nx][ny] != -1) {
+                                    q.offer(new int[]{nx, ny});
+                                    ch[nx][ny] = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            answer = Math.max(answer, cnt);
+
+        }
+        System.out.println(answer);
+
+    }
+}
