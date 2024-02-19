49428ad85fdfcf9faab3240063e09857b5efb21c
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-02-19 15:08:40 +0900

    [Silver II] Title: 베르트랑 공준, Time: 216 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -5,11 +5,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[246913];
+        arr[0] = 0;
+        arr[1] = 0;
+        arr[2] = 1;
+        arr[3] = 1;
+        for (int i = 4; i < 246913; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    arr[i] = 0;
+                    break;
+                }
+                arr[i] = 1;
+            }
+        }
+        
         while (true) {
             int n = Integer.parseInt(br.readLine());
             if (n == 0) break;
+            int cnt = 0;
+            for (int i = n+1; i <= 2*n; i++) {
+                if (arr[i] == 1) cnt++;
+            }
+            System.out.println(cnt);
 
-            System.out.println(cal(n + 1, 2 * n));
+//            System.out.println(cal(n + 1, 2 * n));
         }
 
     }
