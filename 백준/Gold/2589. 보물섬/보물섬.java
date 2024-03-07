ba93bd78812ae34191a65f8626124ec984e61820
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-03-07 20:13:19 +0900

    [Gold V] Title: 보물섬, Time: 488 ms, Memory: 189992 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,56 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr, dist, ch;
-    static Queue<int[]> q = new LinkedList<>();
+    static int[][] ch;
+    static char[][] arr;
+    static Queue<Node> q = new ArrayDeque<>();
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
+    static class Node {
+        int x, y, cnt;
+
+        public Node(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
         int m = Integer.parseInt(s[1]);
-        arr = new int[n][m];
-        dist = new int[n][m];
+        arr = new char[n][m];
         for (int i = 0; i < n; i++) {
-            String x = br.readLine();
+            char[] x = br.readLine().toCharArray();
             for (int j = 0; j < m; j++) {
-                if (x.charAt(j) == 'W') arr[i][j] = 1;
-                else arr[i][j] = 0;
+                arr[i][j] = x[j];
             }
         }
         int answer = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
+                if (arr[i][j] == 'L') {
                     ch = new int[n][m];
                     ch[i][j] = 1;
-                    q.offer(new int[]{i, j});
-                    int L = 0;
+                    q.offer(new Node(i, j, 0));
                     while (!q.isEmpty()) {
-                        L++;
-                        int len = q.size();
-                        for (int k = 0; k < len; k++) {
-                            int[] p = q.poll();
-                            for (int l = 0; l < 4; l++) {
-                                int nx = p[0] + dx[l];
-                                int ny = p[1] + dy[l];
-                                if (nx >= 0 && ny >= 0 && nx < n && ny < m && ch[nx][ny] == 0 && arr[nx][ny] == 0) {
-                                    ch[nx][ny] = 1;
-                                    q.offer(new int[]{nx, ny});
-                                }
+                        Node p = q.poll();
+                        answer = Math.max(answer, p.cnt);
+                        for (int l = 0; l < 4; l++) {
+                            int nx = p.x + dx[l];
+                            int ny = p.y + dy[l];
+                            if (nx >= 0 && ny >= 0 && nx < n && ny < m && ch[nx][ny] == 0 && arr[nx][ny] == 'L') {
+                                ch[nx][ny] = 1;
+                                q.offer(new Node(nx, ny, p.cnt + 1));
                             }
                         }
                     }
-                    answer = Math.max(answer, L);
                 }
             }
         }
-        System.out.println(answer - 1);
+        System.out.println(answer);
     }
 }
