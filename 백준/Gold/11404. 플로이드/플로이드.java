8d52259fb6f97798c20decc4272fa8421a7f532a
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-03-16 14:15:44 +0900

    [Gold IV] Title: 플로이드, Time: 868 ms, Memory: 49388 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,63 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
-
-    static int n, m;
-    static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
-
-    private static int[] dijstra(int start) {
-        int[] res = new int[n + 1];
-        Arrays.fill(res, Integer.MAX_VALUE);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.offer(new int[]{start, 0});
-        while (!pq.isEmpty()) {
-            int[] p = pq.poll();
-            if (p[1] > res[p[0]]) continue;
-            for (int[] x : graph.get(p[0])) {
-                if (x[0] == start) continue;
-                if (res[x[0]] > p[1] + x[1]) {
-                    res[x[0]] = p[1] + x[1];
-                    pq.offer(new int[]{x[0], p[1] + x[1]});
-                }
-            }
-        }
-        return res;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
-
+        StringTokenizer st;
         for (int i = 0; i < m; i++) {
-            String[] s = br.readLine().split(" ");
-            int start = Integer.parseInt(s[0]);
-            int end = Integer.parseInt(s[1]);
-            int cost = Integer.parseInt(s[2]);
-            graph.get(start).add(new int[]{end, cost});
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(x).add(new int[]{y, cost});
         }
-
-
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         for (int i = 1; i <= n; i++) {
-            int[] res = dijstra(i);
-//            StringBuilder sb = new StringBuilder();
-            for (int j = 1; j < res.length; j++) {
-                if (res[j] == Integer.MAX_VALUE) System.out.print(0 + " ");
-                else System.out.print(res[j] + " ");
-//                sb.append(res[j]).append(" ");
+            int[] dist = new int[n + 1];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            pq.offer(new int[]{i, 0});
+            while (!pq.isEmpty()) {
+                int[] p = pq.poll();
+                if (dist[p[0]] < p[1]) continue;
+                for (int[] x : graph.get(p[0])) {
+                    if (dist[x[0]] > x[1] + p[1]) {
+                        dist[x[0]] = x[1] + p[1];
+                        pq.offer(new int[]{x[0], x[1] + p[1]});
+                    }
+                }
             }
-            System.out.println();
+            for (int j = 1; j < dist.length; j++) {
+                if (i == j || dist[j] == Integer.MAX_VALUE) {
+                    sb.append(0).append(" ");
+                    continue;
+                }
+                sb.append(dist[j]).append(" ");
+            }
+            sb.append("\n");
         }
-
-
+        System.out.println(sb);
     }
-
 }
