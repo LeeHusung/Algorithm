66de9589622f49239f77d1d8ab2f2216e7c8c163
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-03-30 16:47:02 +0900

    [Silver III] Title: 수열, Time: 288 ms, Memory: 24084 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -9,21 +9,16 @@ public class Main {
         String[] s = br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
         int k = Integer.parseInt(s[1]);
-        int[] arr = new int[n];
+        int[] arr = new int[n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
 
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n - k + 1; i++) {
-            int x = i;
-            int y = x + k;
-            int sum = 0;
-            for (int j = x; j < y; j++) {
-                sum += arr[j];
-            }
-            max = Math.max(max, sum);
+        for (int i = k; i <= n; i++) {
+            int x = arr[i] - arr[i - k];
+            max = Math.max(max, x);
         }
         System.out.println(max);
 
