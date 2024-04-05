8730b8c76da54e2551d68b97b62c50843f978cc1
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-04-05 13:03:05 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 920 ms, Memory: 106180 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+        arr = new int[n + 1][m + 1];
+        dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            String[] s1 = br.readLine().split("");
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(s1[j - 1]);
+            }
+        }
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (arr[i][j] == 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+
+        System.out.println(max *max);
+
+}
+
+}
