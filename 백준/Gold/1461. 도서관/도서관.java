5cf144dd7864eff4bc4a8d6c63daa7fa0e001f9f
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-04-05 11:42:04 +0900

    [Gold IV] Title: 도서관, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.java
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -1,69 +1,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
         int m = Integer.parseInt(s[1]);
-        int[] arr = new int[n];
-        String[] s1 = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(s1[i]);
-        }
-
         if (n == 1) {
-            System.out.println(Math.abs(arr[0]));
+            System.out.println(Math.abs(Integer.parseInt(br.readLine())));
             return;
         }
-        Arrays.sort(arr);
 
-        LinkedList<Integer> pre = new LinkedList<>();
-        LinkedList<Integer> after = new LinkedList<>();
-        LinkedList<Integer> distance = new LinkedList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) pre.add(arr[i]);
-            else after.add(arr[i]);
+        List<Integer> plus = new ArrayList<>();
+        List<Integer> minus = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (x > 0) plus.add(x);
+            else minus.add(x);
         }
-        Collections.sort(pre, Collections.reverseOrder());
+
+        Collections.sort(plus);
+        Collections.sort(minus, (a, b) -> b - a);
 
         int answer = 0;
-//        if (!pre.isEmpty() && !after.isEmpty() && Math.abs(pre.get(pre.size()-1)) >= Math.abs(after.get(after.size() - 1))) {
-//            answer += Math.abs(pre.get(pre.size()-1));
-//            for (int i = 0; i < m&& !pre.isEmpty(); i++) {
-//                pre.remove(pre.size() - 1);
-//            }
-//        } else if (!after.isEmpty()) {
-//            answer += after.get(after.size() - 1);
-//            for (int i = 0; i < m&& !after.isEmpty(); i++) {
-//                after.remove(after.size() - 1);
-//            }
-//        }
-        while (!after.isEmpty()) {
-            distance.add(after.pollLast());
-            for (int i = 1; i < m && !after.isEmpty(); i++) {
-                after.pollLast();
+        if (minus.isEmpty() && !plus.isEmpty()) {
+            int res = plus.get(plus.size() - 1);
+            while (!plus.isEmpty()) {
+                answer += plus.get(plus.size()-1) * 2;
+                for (int i = 0; i < m; i++) {
+                    plus.remove(plus.size() - 1);
+                    if (plus.isEmpty()) break;
+                }
             }
+            answer -= res;
+        } else if (!minus.isEmpty() && plus.isEmpty()) {
+            int res = -minus.get(minus.size() - 1);
+            while (!minus.isEmpty()) {
+                answer += -minus.get(minus.size()-1) * 2;
+                for (int i = 0; i < m; i++) {
+                    minus.remove(minus.size() - 1);
+                    if (minus.isEmpty()) break;
+                }
+            }
+            answer -= res;
+        } else {
+            int res = 0;
+            if (plus.get(plus.size()-1) > -minus.get(minus.size()-1)) {
+                res = plus.get(plus.size()-1);
+            } else {
+                res = -minus.get(minus.size()-1);
+            }
+            while (!plus.isEmpty()) {
+                answer += plus.get(plus.size() - 1) * 2;
+                for (int i = 0; i < m; i++) {
+                    plus.remove(plus.size() - 1);
+                    if (plus.isEmpty()) break;
+                }
+            }
+            while (!minus.isEmpty()) {
+                answer += -minus.get(minus.size() - 1) * 2;
+                for (int i = 0; i < m; i++) {
+                    minus.remove(minus.size() - 1);
+                    if (minus.isEmpty()) break;
+                }
+            }
+            answer -= res;
         }
 
-        while (!pre.isEmpty()) {
-            distance.add(-pre.pollLast());
-            for (int i = 1; i < m && !pre.isEmpty(); i++) {
-                pre.pollLast();
-            }
-        }
-        Collections.sort(distance);
-        for (int i=0; i<distance.size(); i++) {
-            // 가장 먼 거리만 편도로 이동, 남은 거리는 왕복이라 x2
-            if (i == distance.size()-1) answer += distance.get(i);
-            else answer += (distance.get(i)*2);
-        }
         System.out.println(answer);
 
     }
