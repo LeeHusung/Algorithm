f9620c28c12c248af568b608a234c3104cbd9c6f
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-04-07 12:33:45 +0900

    [Gold III] Title: 합, Time: 140 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/1132. 합/합.java
+++ 백준/Gold/1132. 합/합.java
@@ -18,12 +18,10 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         Map<Character, Long> map = new HashMap<>();
         Map<Character, Long> res = new HashMap<>();
-        Set<Character> noFirst = new HashSet<>();
         List<String> list = new ArrayList<>();
         List<Point> points = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            noFirst.add(s.charAt(0));
             list.add(s);
             long x = 1;
             for (int j = s.length()-1; j >=0; j--) {
@@ -31,22 +29,25 @@ public class Main {
                 x *= 10;
             }
         }
-        for (Character x : map.keySet()) {
-            points.add(new Point(x, map.get(x)));
-        }
-        Collections.sort(points, (a, b) -> Long.compare(b.value, a.value));
 
+        List<Character> noPossible = new ArrayList<>();
         List<Character> possible = new ArrayList<>();
 
         if (map.size() == 10) {
+            for (String s : list) {
+                noPossible.add(s.charAt(0));
+            }
             for (char ch = 'A'; ch <= 'J'; ch++) {
-                if (!noFirst.contains(ch)) {
+                if (!noPossible.contains(ch)) {
                     possible.add(ch);
                 }
             }
             long num = 9;
-
+            for (Character x : map.keySet()) {
+                points.add(new Point(x, map.get(x)));
+            }
             char zero = 0;
+            Collections.sort(points, (a, b) -> Long.compare(b.value, a.value));
             for (int i = points.size()-1; i >= 0; i--) {
                 if (possible.contains(points.get(i).key)) {
                     res.put(points.get(i).key, 0L);
@@ -60,10 +61,15 @@ public class Main {
             }
         } else {
             long num = 9;
+            for (Character x : map.keySet()) {
+                points.add(new Point(x, map.get(x)));
+            }
+            Collections.sort(points, (a, b) -> Long.compare(b.value, a.value));
             for (Point point : points) {
                 res.put(point.key, num--);
             }
         }
+
         long answer = 0;
         for (String s : list) {
             String str = "";
