95c776389993b7f4b72502b1150944b4a99336ea
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-04-11 11:59:35 +0900

    [Gold III] Title: 아기 상어, Time: 140 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,12 +11,13 @@ public class Main {
     static int[] dy = {0, -1, 0, 1};
     static int[][] arr, ch;
     static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] == b[2] ? (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]) : a[2] - b[2]);
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
         ch = new int[n][n];
-        start = new int[2];
+        start = new int[3];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -27,6 +26,7 @@ public class Main {
                 if (arr[i][j] == 9) {
                     start[0] = i;
                     start[1] = j;
+                    arr[i][j] = 0;
                 }
             }
         }
@@ -35,47 +35,38 @@ public class Main {
         int cnt = 0;
 
         while (true) {
-            if (!canEat()) break;
-            int[] next = pq.poll();
+            int[] next = canEat();
+            if (next == null) break;
             time += next[2];
-            arr[start[0]][start[1]] = 0;
             cnt++;
+            start[0] = next[0];
+            start[1] = next[1];
+            arr[next[0]][next[1]] = 0;
             if (sharkSize == cnt) {
                 sharkSize++;
                 cnt = 0;
             }
-            start = next;
             pq.clear();
         }
         System.out.println(time);
     }
 
-    private static boolean canEat() {
-        boolean flag = false;
-        int dist = 0;
-        Queue<int[]> q = new ArrayDeque<>();
+    private static int[] canEat() {
+        pq.offer(new int[]{start[0], start[1], 0});
         ch = new int[n][n];
-        q.offer(start);
         ch[start[0]][start[1]] = 1;
-        while (!q.isEmpty()) {
-            int len = q.size();
-            dist++;
-            for (int i = 0; i < len; i++) {
-                int[] p = q.poll();
-                for (int j = 0; j < 4; j++) {
-                    int nx = p[0] + dx[j];
-                    int ny = p[1] + dy[j];
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] <= sharkSize && ch[nx][ny] == 0) { //지나갈 수 있음
-                        if (arr[nx][ny] > 0 && arr[nx][ny] < sharkSize) { //먹을 수 있음
-                            pq.offer(new int[]{nx, ny, dist});
-                            flag = true;
-                        }
-                        ch[nx][ny] = 1;
-                        q.offer(new int[]{nx, ny});
-                    }
+        while (!pq.isEmpty()) {
+            int[] p = pq.poll();
+            if (arr[p[0]][p[1]] > 0 && arr[p[0]][p[1]] < sharkSize) return p;
+            for (int i = 0; i < 4; i++) {
+                int nx = p[0] + dx[i];
+                int ny = p[1] + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] <= sharkSize && ch[nx][ny] == 0) { //지나갈 수 있음
+                    pq.offer(new int[]{nx, ny, p[2] + 1});
+                    ch[nx][ny] = 1;
                 }
             }
         }
-        return flag;
+        return null;
     }
 }
