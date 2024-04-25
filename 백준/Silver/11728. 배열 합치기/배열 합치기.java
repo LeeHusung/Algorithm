b3c942627df59c62a278bbecaf8e3b7156465656
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-04-25 15:09:54 +0900

    [Silver V] Title: 배열 합치기, Time: 1540 ms, Memory: 317096 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -11,38 +11,17 @@ public class Main {
         int m = Integer.parseInt(s[1]);
         String[] s1 = br.readLine().split(" ");
         String[] s2 = br.readLine().split(" ");
-        int[] arr = new int[n];
+        int[] arr = new int[n + m];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(s1[i]);
         }
-        int[] brr = new int[m];
-        for (int i = 0; i < m; i++) {
-            brr[i] = Integer.parseInt(s2[i]);
+        for (int i = n; i < n + m; i++) {
+            arr[i] = Integer.parseInt(s2[i - n]);
         }
-
         Arrays.sort(arr);
-        Arrays.sort(brr);
-        int x = 0;
-        int y = 0;
         StringBuilder sb = new StringBuilder();
-        while (true) {
-            if (x >= n && y >= m) break;
-            if (arr[x] > brr[y]) {
-                sb.append(brr[y]).append(" ");
-                y++;
-            } else {
-                sb.append(arr[x]).append(" ");
-                x++;
-            }
-            if (x >= n) {
-                while (y < m) {
-                    sb.append(brr[y++]).append(" ");
-                }
-            } else if (y >= m) {
-                while (x < n) {
-                    sb.append(arr[x++]).append(" ");
-                }
-            }
+        for (int i : arr) {
+            sb.append(i).append(" ");
         }
         System.out.println(sb);
 
