2a573cb1a63e2342af167a48bd2ebc175d8eb516
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-04-29 11:19:49 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.53 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,66 +1,44 @@
 import java.util.*;
 class Solution {
-    
     public int[] solution(int[] fees, String[] records) {
-
+      
         Map<String, Integer> inMap = new HashMap<>();
-        Map<String, Integer> parkMap = new HashMap<>();
+        TreeMap<String, Integer> resMap = new TreeMap<>();
         for (String x : records) {
-            String[] one = x.split(" ");
-            if (one[2].equals("IN")) {
-                inMap.put(one[1], getTime(one[0]));
-            }
-            if (one[2].equals("OUT")) {
-                for (String key : inMap.keySet()) {
-                    if (one[1].equals(key)) {
-                        int parkTime = getTime(one[0]) - inMap.get(key);
-                        parkMap.put(key, parkMap.getOrDefault(key, 0) + parkTime);
-                        inMap.put(key, 1000000);
-                        // System.out.println(key + " " + parkTime);
-                    }
-                }
+            String[] split = x.split(" ");
+            if (split[2].equals("IN")) {
+                inMap.put(split[1], getTime(split[0]));
+            } else if (split[2].equals("OUT")) {
+                resMap.put(split[1], resMap.getOrDefault(split[1], 0) + getTime(split[0]) - inMap.get(split[1]));
+                inMap.remove(split[1]);
             }
         }
-    
         for (String key : inMap.keySet()) {
-            if (inMap.get(key) != 1000000) {
-                parkMap.put(key, parkMap.getOrDefault(key, 0) + getTime("23:59") - inMap.get(key));
-            }
+            resMap.put(key, resMap.getOrDefault(key, 0) + getTime("23:59") - inMap.get(key));
         }
-    
-        
-        Map<String, Integer> resultMap = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        for (String key : parkMap.keySet()) {
-            list.add(key);
-            if (parkMap.get(key) <= fees[0]) {
-                resultMap.put(key, fees[1]);
-            } else {
-                double ceil = Math.ceil((double)(parkMap.get(key) - fees[0]) / fees[2]);
-                // System.out.println(ceil);
-                int fee = fees[1] + (int) ceil * fees[3];
-                resultMap.put(key, fee);
-            }
-        }
-            
-//         for (String key : resultMap.keySet()) {
-//             System.out.println(key + " " + resultMap.get(key));
-//         }
-        Collections.sort(list);
-        
-        int[] answer = new int[resultMap.size()];
         int idx = 0;
-        for (String key : list) {
-            answer[idx++] = resultMap.get(key);
+        int[] answer = new int[resMap.size()];
+        for (String key : resMap.keySet()) {
+            int sum = 0;
+            double parkTime = resMap.get(key);
+            // System.out.println(parkTime);
+            if (parkTime <= fees[0]) {
+                answer[idx++] = fees[1];
+                continue;
+            }
+            if (parkTime > fees[0]) {
+                sum += fees[1];
+            }
+            sum += Math.ceil((parkTime - fees[0]) / fees[2]) * fees[3];
+            answer[idx++] = sum;
         }
         
         return answer;
     }
-    
     public int getTime(String time) {
-        String[] s = time.split(":");
-        int h = Integer.parseInt(s[0]) * 60;
-        int m = Integer.parseInt(s[1]);
+        String[] tSplit = time.split(":");
+        int h = Integer.parseInt(tSplit[0]) * 60;
+        int m = Integer.parseInt(tSplit[1]);
         return h + m;
     }
 }
