307dc8ad794608066ebe0ae844611d0b0bf06539
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-05-07 18:48:36 +0900

    [Gold II] Title: 청소년 상어, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -1,10 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,115 +9,6 @@ public class Main {
     static int[][][] arr;
     static int[] dx = {0, -1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, 0, -1, -1, -1, 0, 1, 1, 1};
-
-    public static int[][][] moveFish(int map[][][]) {
-        int count =0;
-
-        int clone[][][]=new int[4][4][2];
-        for(int i=0;i<4;i++) {
-            for(int j=0;j<4;j++) {
-                clone[i][j][0] =map[i][j][0];
-                clone[i][j][1] =map[i][j][1];
-                if(clone[i][j][0]>0)count ++;
-            }
-        }
-
-        HashSet<Integer> set=new HashSet<>();
-
-
-        int dir = 0;
-        while(count-->0) {
-
-            int min = Integer.MAX_VALUE;
-            int yy=0;
-            int xx=0;
-            for(int i=0;i<4;i++) {
-                for(int j=0;j<4;j++) {
-                    if(clone[i][j][0]>0&&
-                            clone[i][j][0]<min&&
-                            !set.contains(clone[i][j][0])) {
-                        //최소 물고기 번호 갱신.
-                        min = clone[i][j][0];
-                        dir = clone[i][j][1];
-                        xx=i;
-                        yy=j;
-                    }
-                }
-            }
-            set.add(min);
-            int nextY=0;
-            int nextX=0;
-            while(true) {
-                nextY = yy + dy[dir];
-                nextX = xx + dx[dir];
-                if(nextY<0||nextX<0||nextY>=4||nextX>=4)
-                    dir ++;
-                else if(clone[nextX][nextY][0]== -1)
-                    dir++;
-                else
-                    break;
-
-                if(dir>8)
-                    dir=1;
-            }
-            int temp1 = clone[xx][yy][0];
-            int temp2 = dir;
-            clone[xx][yy][0] = clone[nextX][nextY][0];
-            clone[xx][yy][1] = clone[nextX][nextY][1];
-            clone[nextX][nextY][0] =temp1;
-            clone[nextX][nextY][1] =temp2;
-
-        }
-
-        return clone;
-
-
-
-    }
-
-    private static void D(int[][][] arr, int sum) {
-        answer = Math.max(answer, sum);
-
-        int[][][] copy = moveFish(arr);
-
-        int sharkX = 0;
-        int sharkY = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (copy[i][j][0] == -1) {
-                    sharkX = i;
-                    sharkY = j;
-                }
-            }
-        }
-        int preX = sharkX;
-        int preY = sharkY;
-
-        int dir = copy[sharkX][sharkY][1];
-
-        for (int p = 0; p < 3; p++) {
-            int[][][] newArr = new int[4][4][2];
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    newArr[i][j] = copy[i][j].clone();
-                }
-            }
-
-            sharkX += dx[dir];
-            sharkY += dy[dir];
-            if (sharkX < 0 || sharkY < 0 || sharkX >= 4 || sharkY >= 4) break;
-            if (newArr[sharkX][sharkY][0] > 0) {
-                newArr[preX][preY][0] = 0;
-                newArr[preX][preY][1] = 0;
-                int temp = newArr[sharkX][sharkY][0];
-                newArr[sharkX][sharkY][0] = -1;
-                D(newArr, sum +temp);
-            }
-        }
-
-    }
-
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -139,7 +27,92 @@ public class Main {
         answer = sum;
         D(arr, sum);
         System.out.println(answer);
+
     }
 
+    private static void D(int[][][] arr, int sum) {
+        answer = Math.max(answer, sum);
+        int[][][] copy = moveFish(arr);
+
+        int sharkX = 0, sharkY = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (copy[i][j][0] == -1) {
+                    sharkX = i;
+                    sharkY = j;
+                }
+            }
+        }
+
+        int dir = copy[sharkX][sharkY][1];
+        int preX = sharkX, preY = sharkY;
+        for (int i = 0; i < 3; i++) {
+            int[][][] newArr = new int[4][4][2];
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    newArr[j][k] = copy[j][k].clone();
+                }
+            }
+
+            sharkX += dx[dir];
+            sharkY += dy[dir];
+            if (sharkX >= 4 || sharkY >= 4 || sharkX < 0 || sharkY < 0) break;
+            if (newArr[sharkX][sharkY][0] > 0) {
+                newArr[preX][preY][0] = 0;
+                newArr[preX][preY][1] = 0;
+                int temp = newArr[sharkX][sharkY][0];
+                newArr[sharkX][sharkY][0] = -1;
+                D(newArr, sum + temp);
+            }
+        }
+
+    }
+
+    private static int[][][] moveFish(int[][][] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        int[][][] copy = new int[4][4][2];
+        int cnt = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                copy[i][j][0] = arr[i][j][0];
+                copy[i][j][1] = arr[i][j][1];
+                if (copy[i][j][0] > 0) cnt++;
+            }
+        }
+
+        int dir = 0;
+        while (cnt-- > 0) {
+            int min = Integer.MAX_VALUE;
+            int x = 0, y = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (copy[i][j][0] > 0 && copy[i][j][0] < min && !set.contains(copy[i][j][0])) {
+                        min = copy[i][j][0];
+                        x = i;
+                        y = j;
+                        dir = copy[i][j][1];
+                    }
+                }
+            }
+            set.add(min);
+            int nx = 0, ny = 0;
+            while (true) {
+                nx = x + dx[dir];
+                ny = y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= 4 || ny >= 4 || copy[nx][ny][0] == -1) dir++;
+                else {
+                    break;
+                }
+                if (dir > 8) dir = 1;
+            }
+            int temp1 = copy[x][y][0];
+            int temp2 = dir;
+            copy[x][y][0] = copy[nx][ny][0];
+            copy[x][y][1] = copy[nx][ny][1];
+            copy[nx][ny][0] =temp1;
+            copy[nx][ny][1] =temp2;
+        }
+        return copy;
+    }
 
 }
