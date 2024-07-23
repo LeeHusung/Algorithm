bd233e2b606a58341b48cc156cd2761d620350e7
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-07-23 13:41:04 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 996 ms, Memory: 70760 KB -BaekjoonHub

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
@@ -2,64 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Main {
-    static class Node {
-        int x; int y; int seat;
-
-        public Node(final int x, final int y, final int seat) {
-            this.x = x;
-            this.y = y;
-            this.seat = seat;
-        }
-    }
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] seat = new int[n];
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparing(e -> e.y));
-        PriorityQueue<Integer> remains = new PriorityQueue<>();
-        PriorityQueue<Node> arr = new PriorityQueue<>(Comparator.comparing(e -> e.x));
-        int idx = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        int[][] arr = new int[n][2];
+        int[] seat = new int[n + 1];
+        int idx = 1;
         for (int i = 0; i < n; i++) {
             String[] s = br.readLine().split(" ");
             int start = Integer.parseInt(s[0]);
             int end = Integer.parseInt(s[1]);
-            arr.offer(new Node(start, end, 0));
+            arr[i][0] = start;
+            arr[i][1] = end;
         }
-        while (!arr.isEmpty()) {
-            Node node = arr.poll();
-            if (pq.isEmpty()) {
-                pq.offer(new Node(node.x, node.y, idx));
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        for (int i = 0; i < n; i++) {
+            int start = arr[i][0];
+            int end = arr[i][1];
+            if (q.isEmpty()) {
+                q.offer(new int[]{start, end, idx});
                 seat[idx++]++;
                 continue;
             }
-
-            while (!pq.isEmpty()) {
-                if (pq.peek().y < node.x) {
-                    Node poll = pq.poll();
-                    remains.add(poll.seat);
+            while (!q.isEmpty()) {
+                if (q.peek()[1] <= start) {
+                    int[] poll = q.poll();
+                    pq.offer(poll[2]);
                 } else break;
             }
-            if (remains.isEmpty()) {
-                pq.offer(new Node(node.x, node.y, idx));
-                seat[idx++]++;
+
+            if (!pq.isEmpty()) {
+                Integer p = pq.poll();
+                q.offer(new int[]{start, end, p});
+                seat[p]++;
             } else {
-                Integer seatno = remains.poll();
-                pq.offer(new Node(node.x, node.y, seatno));
-                seat[seatno]++;
+                q.offer(new int[]{start, end, idx});
+                seat[idx++]++;
             }
         }
-
-        System.out.println(idx);
-        for (int i = 0; i < idx; i++) {
-            System.out.print(seat[i] + " ");
+        idx--;
+        StringBuilder sb = new StringBuilder();
+        sb.append(idx).append("\n");
+        for (int i = 1; i <= idx; i++) {
+            sb.append(seat[i]).append(" ");
         }
+        System.out.println(sb);
     }
 }
