6d97939e0916b5bf6eeb1db565f05fb6accd2d5b
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-07-30 13:28:46 +0900

    [Gold V] Title: 크리보드, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Gold/11058. 크리보드/크리보드.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n <= 6) {
+            System.out.println(n);
+            return;
+        }
+        long[] dp = new long[n + 1];
+        for (int i = 1; i <= 6; i++) {
+            dp[i] = i;
+        }
+
+        for (int i = 7; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 5] * 4, Math.max(dp[i - 4] * 3, dp[i - 3] * 2));
+        }
+        System.out.println(dp[n]);
+    }
+}
