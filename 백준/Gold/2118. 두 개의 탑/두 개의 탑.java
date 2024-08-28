e7b1a468a7f9a8d270d5bedc312dd9f8d2cb3a0a
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-08-28 12:14:45 +0900

    [Gold V] Title: 두 개의 탑, Time: 144 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -6,29 +6,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int[] arr = new int[n + 1];
         int k = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             k += arr[i];
         }
-        int[] sum = new int[n];
-        sum[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            sum[i] = sum[i - 1] + arr[i];
-        }
 
-        int max = Integer.MIN_VALUE;
-        max = Math.max(max, Math.min(sum[0], k - sum[0]));
-
-        for (int i = 1; i < n; i++) {
-            int x = sum[i];
-            for (int j = i - 1; j >= 0; j--) {
-                int y = sum[j];
-                max = Math.max(max, Math.min(x - y, k - (x - y)));
+        int start = 0;
+        int end = 0;
+        int now = arr[start];
+        int max = 0;
+        while (start <= end && end < n) {
+            int minNow = Math.min(now, k - now);
+            max = Math.max(max, minNow);
+            if (minNow == now) {
+                end++;
+                now += arr[end];
+            } else {
+                now -= arr[start];
+                start++;
             }
         }
-
         System.out.println(max);
+
+
+
     }
 }
