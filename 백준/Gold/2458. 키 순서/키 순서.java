dc64f9deb3cf9dff57ac9f5e84d62b0591f4f0a6
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-11-10 13:17:04 +0900

    [Gold IV] Title: 키 순서, Time: 756 ms, Memory: 63556 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,58 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> reverseGraph = new ArrayList<>();
+
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
-        }
-        int[][] arr = new int[m][2];
-        int[] cost = new int[n + 1];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            graph.get(arr[i][0]).add(arr[i][1]);
+            reverseGraph.add(new ArrayList<>());
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            String[] s = br.readLine().split(" ");
+            int a = Integer.parseInt(s[0]);
+            int b = Integer.parseInt(s[1]);
+            graph.get(a).add(b);
+            reverseGraph.get(b).add(a);
+        }
+
+        int res = 0;
+        int[] cnt = new int[n + 1];
+        Queue<Integer> q = new ArrayDeque<>();
         for (int i = 1; i <= n; i++) {
             int[] ch = new int[n + 1];
-            q.offer(i);
+            int tmp = 0;
             ch[i] = 1;
+            q.offer(i);
             while (!q.isEmpty()) {
                 Integer p = q.poll();
-                for (int x : graph.get(p)) {
+                for (Integer x : graph.get(p)) {
                     if (ch[x] == 0) {
+                        tmp++;
                         ch[x] = 1;
-                        cost[i]++;
-                        cost[x]++;
                         q.offer(x);
                     }
                 }
             }
+            q.clear();
+            q.offer(i);
+            while (!q.isEmpty()) {
+                Integer p = q.poll();
+                for (Integer x : reverseGraph.get(p)) {
+                    if (ch[x] == 0) {
+                        tmp++;
+                        ch[x] = 1;
+                        q.offer(x);
+                    }
+                }
+            }
+            cnt[i] = tmp;
         }
-        int answer = 0;
-        for (int x : cost) {
-//            System.out.print(x + " ");
-            if (x == n-1) answer++;
+
+        for (int x : cnt) {
+            if (x == n - 1) res++;
         }
-//        System.out.println();
-        System.out.println(answer);
-
-
-
-
-
+        System.out.println(res);
     }
 }
