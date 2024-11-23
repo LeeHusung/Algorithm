ada6a9e4f25db1e90eac605b007df0e3cc90827a
LHS <128116509+LeeHusung@users.noreply.github.com>
2024-11-23 19:51:44 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 13608 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
@@ -14,7 +14,6 @@ public class Main {
         int[][] ch = new int[n][m];
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, -1, 0, 1};
-
         for (int i = 0; i < n; i++) {
             String[] s1 = br.readLine().split("");
             for (int j = 0; j < m; j++) {
@@ -22,32 +21,27 @@ public class Main {
             }
         }
 
-        System.out.println(bfs(n, m, arr, ch, dx, dy));
-
-    }
-
-    private static int bfs(int n, int m, int[][] arr, int[][] ch, int[] dx, int[] dy) {
-        Queue<int[]> q = new LinkedList<>();
-        int L = 0;
+        Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{0, 0});
         ch[0][0] = 1;
+        int L = 0;
         while (!q.isEmpty()) {
-            int len = q.size();
             L++;
+            int len = q.size();
             for (int i = 0; i < len; i++) {
                 int[] p = q.poll();
-                if (p[0] == n - 1 && p[1] == m - 1) return L;
+                if (p[0] == n - 1 && p[1] == m - 1) {
+                    System.out.println(L);
+                }
                 for (int j = 0; j < 4; j++) {
                     int nx = p[0] + dx[j];
                     int ny = p[1] + dy[j];
                     if (nx >= 0 && ny >= 0 && nx < n && ny < m && ch[nx][ny] == 0 && arr[nx][ny] == 1) {
-                        ch[nx][ny] = 1;
                         q.offer(new int[]{nx, ny});
+                        ch[nx][ny] = 1;
                     }
                 }
             }
-
         }
-        return 0;
     }
 }
