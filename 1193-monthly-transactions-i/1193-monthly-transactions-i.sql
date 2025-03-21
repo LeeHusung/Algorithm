13149eb7c2aa148de6b264c7694ab6f867fbb37c
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-03-21 22:33:39 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,13 @@
+SELECT T1.month, T1.country, T1.trans_count, NVL(T2.approved_count, 0) approved_count, T1.trans_total_amount, NVL(T2.approved_total_amount, 0) approved_total_amount
+FROM (
+    SELECT TO_CHAR(TRANS_DATE, 'YYYY-MM') month, country, COUNT(ID) trans_count, SUM(AMOUNT) trans_total_amount
+    FROM Transactions 
+    GROUP BY TO_CHAR(TRANS_DATE, 'YYYY-MM'), COUNTRY
+) T1
+LEFT JOIN (
+    SELECT TO_CHAR(t1.TRANS_DATE, 'YYYY-MM') month, t1.country, count(t1.ID) approved_count, sum(t1.amount) approved_total_amount
+    FROM Transactions t1
+    where t1.state = 'approved'
+    GROUP BY TO_CHAR(t1.TRANS_DATE, 'YYYY-MM'), t1.COUNTRY
+) T2
+ON T1.month = t2.month and T2.country = t1.country
