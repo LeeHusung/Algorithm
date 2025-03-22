718bdce1bf9abd074c043c3485e18a1a468f43f1
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-03-22 17:54:01 +0900

    [Gold V] Title: 택배 배송, Time: 540 ms, Memory: 175192 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph.get(a).add(new int[]{b, c});
+            graph.get(b).add(new int[]{a, c});
+        }
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{1, 0});
+        int[] ch = new int[n + 1];
+        int[] arr = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            arr[i] = Integer.MAX_VALUE;
+        }
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            if (cur[1] > arr[cur[0]]) continue;
+            for (int[] x : graph.get(cur[0])) {
+                if (arr[x[0]] > cur[1] + x[1]) {
+                    arr[x[0]] = cur[1] + x[1];
+                    q.offer(new int[]{x[0], x[1] + cur[1]});
+                }
+            }
+        }
+
+        System.out.println(arr[n]);
+
+    }
+}
