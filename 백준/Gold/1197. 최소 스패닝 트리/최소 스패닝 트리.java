cf0b21b465336a30a510d146e5309473f3831375
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-04-09 08:28:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 572 ms, Memory: 53040 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,58 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int v, e;
     static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
-        int v = Integer.parseInt(s[0]);
-        int e = Integer.parseInt(s[1]);
+        v = Integer.parseInt(s[0]);
+        e = Integer.parseInt(s[1]);
         parents = new int[v + 1];
         for (int i = 1; i < v + 1; i++) {
             parents[i] = i;
         }
-        List<int[]> list = new ArrayList<>();
+
+        int[][] arr = new int[e][3];
         for (int i = 0; i < e; i++) {
-            String[] s1 = br.readLine().split(" ");
-            int x = Integer.parseInt(s1[0]);
-            int y = Integer.parseInt(s1[1]);
-            int cost = Integer.parseInt(s1[2]);
-            list.add(new int[]{x, y, cost});
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr[i][0] = a;
+            arr[i][1] = b;
+            arr[i][2] = c;
         }
-        Collections.sort(list, (a, b) -> a[2] - b[2]);
-        int answer = 0;
-        int cnt = 0;
-        for (int i = 0; i < list.size(); i++) {
-            int[] p = list.get(i);
-            if (find(p[0]) == find(p[1])) continue;
-            answer += p[2];
-            cnt++;
-            if (cnt == v - 1) break;
-            union(p[0], p[1]);
+        Arrays.sort(arr, (a, b) -> a[2] - b[2]);
+
+        long sum = 0;
+        for (int[] x : arr) {
+            int a = x[0];
+            int b = x[1];
+            if (find(a) != find(b)) {
+                sum += x[2];
+                union(a, b);
+            }
         }
-        System.out.println(answer);
+        System.out.println(sum);
     }
 
     private static void union(int x, int y) {
         int fx = find(x);
         int fy = find(y);
+
         if (fx > fy) {
             parents[fx] = fy;
-        } else {
-            parents[fy] = fx;
-        }
+        } else parents[fy] = fx;
     }
 
-    public static int find(int x) {
-        if (x == parents[x]) {
-            return x;
-        }
-        return find(parents[x]);
+  private static int find(int x) {
+    if (x != parents[x]) {
+        parents[x] = find(parents[x]);  // 경로 압축
     }
+    return parents[x];
+}
+
 }
