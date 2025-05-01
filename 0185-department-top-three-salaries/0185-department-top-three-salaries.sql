325f5b271c08e47fc735e79d3d8aa383c4ed5347
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-05-01 11:53:05 +0900

    Time: 882 ms (65.02%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,10 +1,8 @@
 select t2.name Department, t1.name Employee, t1.salary Salary
-from Employee t1
-    join Department t2
-    on t1.departmentId = t2.id
-where 3 > (
-    select count(distinct salary)
-    from Employee t3
-    where t3.departmentId = t1.departmentId
-    and t1.salary < t3.salary
-)
+from (
+select departmentId, name, salary, dense_rank() over (partition by departmentId order by salary desc) rank
+from Employee
+) t1
+join Department t2
+on t1.departmentId = t2.id
+where t1.rank <= 3
