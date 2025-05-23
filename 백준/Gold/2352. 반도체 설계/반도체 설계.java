654184471e314a8235daea274f8fd5fbaf19ac59
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-05-23 09:30:04 +0900

    [Gold II] Title: 반도체 설계, Time: 184 ms, Memory: 19348 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -1,38 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
+    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 1];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int answer = 0;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int max = 0;
-            for (int j = i - 1; j >= 1; j--) {
-                if (arr[i] > arr[j]) {
-                    max = Math.max(max, dp[j]);
+        list.add(arr[0]);
+
+        for (int i = 0; i < n; i++) {
+            if (list.get(list.size() - 1) < arr[i]) {
+                list.add(arr[i]);
+            } else{
+                int left = 0;
+                int right = list.size();
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (list.get(mid) < arr[i]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
                 }
-            }
-            if (max != 0) {
-                dp[i] = max + 1;
-            } else {
-                dp[i] = 1;
+                list.set(right, arr[i]);
             }
         }
-        for (int i = 1; i <= n; i++) {
-            answer = Math.max(answer, dp[i]);
-        }
-        System.out.println(answer);
+        System.out.println(list.size());
+
+
+
 
     }
 }
