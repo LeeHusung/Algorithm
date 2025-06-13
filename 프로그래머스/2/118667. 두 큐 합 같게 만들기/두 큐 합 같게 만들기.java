8a013b17522bf8cb862bdb0e8ad4b50b3869eaf0
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-06-13 14:49:36 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 56.14 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -1;
+        Queue<Integer> a = new ArrayDeque<>();
+        Queue<Integer> b = new ArrayDeque<>();
+        
+        long sum = 0;
+        for (int x : queue1) {
+            sum += x;
+            a.offer(x);
+        }
+        for (int x : queue2) {
+            sum += x;
+            b.offer(x);
+        }
+        if (sum % 2 == 1) {
+            return -1;
+        }
+        long goal = sum / 2;
+        
+        long aSum = 0;
+         for (int x : queue1) {
+            aSum += x;
+        }
+        
+        long bSum = 0;
+         for (int x : queue2) {
+            bSum += x;
+        }
+        int cnt = 0;
+        while (cnt <= queue1.length * 3) {
+            if (aSum == goal) {                
+                return cnt;
+            }
+            if (aSum < goal) {
+                int p = b.poll();
+                a.offer(p);
+                cnt++;
+                bSum -= p;
+                aSum += p;
+            }
+            if (aSum == goal) {                
+                return cnt;
+            }
+             if (aSum > goal) {
+                int p = a.poll();
+                b.offer(p);
+                cnt++;
+                aSum -= p;
+                bSum += p;
+            }
+            if (aSum == goal) {                
+                return cnt;
+            }
+        }
+        
+        return answer;
+    }
+}
