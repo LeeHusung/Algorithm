0568ad37851d6d4b430912d426321d055019e60c
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-10-22 09:29:23 +0900

    Time: 355 ms (31.58%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,6 @@
-select case when mod(id, 2) != 0 and id != counts then id + 1
-            when mod(id, 2) != 0 and id = counts then id
-            else id - 1 end as id,
-            student
-from seat, (select count(*) as counts from seat) as seat_counts
-order by 1;
+select case when id % 2 = 1 and id + 1 in (select id from seat) then id + 1
+            when id % 2 = 0 then id - 1
+            else id
+             end as id, student
+from seat 
+order by id;
