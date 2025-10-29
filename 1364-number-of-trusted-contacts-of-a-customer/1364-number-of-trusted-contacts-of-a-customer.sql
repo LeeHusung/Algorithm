4edd90a38542bffe92c197daf4c6e3def27c61a6
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-10-29 13:30:11 +0900

    Time: 1810 ms (8.37%), Space: 0B (100%) - LeetHub

--- 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
+++ 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
@@ -1,38 +1,18 @@
-select i.invoice_id, c1.customer_name, i.price, ifnull(t2.cnt, 0) contacts_cnt, ifnull(t3.cnt, 0) trusted_contacts_cnt
+-- with cte as (
+--     select user_id, contact_email, sum(contact_email in (select email from customers)) contacts_cnt
+--     from contacts c1
+--     group by user_id
+-- )
+
+select i.invoice_id, t1.customer_name, i.price, ifnull(contacts_cnt, 0) contacts_cnt, ifnull(trusted_contacts_cnt, 0) trusted_contacts_cnt
 from invoices i
-join customers c1
- on i.user_id = c1.customer_id
+left join customers t1
+ on i.user_id = t1.customer_id
 left join (
-    select c1.customer_id, count(c1.customer_id) cnt
-    from customers c1
-    join contacts c2
-    on c1.customer_id = c2.user_id
-    group by c1.customer_id
-) t2
- on t2.customer_id = i.user_id
-left join (
-    select tmp2.customer_id, count(*) cnt
-    from customers tmp1
-    join (
-    select *
-        from customers c1
-        left join contacts c2 
-        on c1.customer_id = c2.user_id 
-    ) tmp2
-    on tmp1.customer_name = tmp2.contact_name
-    group by tmp2.user_id
-) t3
- on t3.customer_id = i.user_id
- order by invoice_id
+            select user_id, contact_email, count(c1.user_id) contacts_cnt, sum(contact_email in (select email from customers)) trusted_contacts_cnt
+        from contacts c1
+        group by user_id
+) tmp
+ on t1.customer_id = tmp.user_id
 
-
--- select tmp2.customer_id, count(*) cnt
--- from customers tmp1
--- join (
---    select *
---     from customers c1
---     left join contacts c2 
---     on c1.customer_id = c2.user_id 
--- ) tmp2
---  on tmp1.customer_name = tmp2.contact_name
---  group by tmp2.user_id
+ order by 1;
