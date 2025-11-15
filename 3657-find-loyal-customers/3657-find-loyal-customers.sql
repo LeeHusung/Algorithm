c631642621e52c185bd7f837611549c0e987c79d
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-11-15 14:59:30 +0900

    Time: 336 ms (18.54%), Space: 0B (100%) - LeetHub

--- 3657-find-loyal-customers/3657-find-loyal-customers.sql
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -8,20 +8,14 @@ from (
 ) t3
 join (
     select customer_id
-    from customer_transactions
-    group by customer_id
-    having datediff(max(transaction_date), min(transaction_date)) >= 30
-) t5
-on t3.customer_id = t5.customer_id
-join (
-    select t1.customer_id
     from (
         select customer_id, (select count(transaction_id) from customer_transactions where transaction_type = 'refund' and customer_id = tmp.customer_id) / 
-        (select count(transaction_id) from customer_transactions where customer_id = tmp.customer_id) cnt
+            (select count(transaction_id) from customer_transactions where customer_id = tmp.customer_id) cnt
         from customer_transactions tmp
-        group by customer_id
+        group by customer_id 
+        having datediff(max(transaction_date), min(transaction_date)) >= 30
     ) t1
     where t1.cnt < 0.2
-) t4
-on t3.customer_id = t4.customer_id
+) t5
+on t3.customer_id = t5.customer_id
 order by t3.customer_id
