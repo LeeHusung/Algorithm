e3d7a98c712fab1e9a6763361fa6448f2f27e202
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-11-19 08:37:02 +0900

    Time: 41 ms (23.67%), Space: 60 MB (5.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,6 +5,7 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int l = i + 1;
             int r = n - 1;
             while (l < r) {
