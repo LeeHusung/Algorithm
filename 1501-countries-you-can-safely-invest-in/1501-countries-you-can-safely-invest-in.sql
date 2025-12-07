c865851bfa97a6744047da7335dc318512345f6a
LHS <128116509+LeeHusung@users.noreply.github.com>
2025-12-07 15:02:40 +0900

    Time: 923 ms (96.8%), Space: 0B (100%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -1,5 +1,5 @@
 with avg as(
-    select format(2 * sum(duration) / (count(duration) * 2), 4) avg
+    select 2 * sum(duration) / (count(duration) * 2) avg
     from calls
 )
 , temp as (
@@ -9,20 +9,22 @@ with avg as(
     on c.country_code = SUBSTRING_INDEX(p.phone_number, '-', 1) 
 )
 
+
 select t1.country_name country
 from (
-    select t.country_name, count(c.caller_id) cnt, sum(c.duration) sum
+    select t.country_name, count(c1.caller_id) cnt, ifnull(sum(c1.duration), 0) sum
     from temp t
-    join calls c
-    on t.id = c.caller_id
+    left join calls c1
+    on t.id = c1.caller_id
     group by t.country_name
 ) t1
-join (
-    select t.country_name, count(c.caller_id) cnt, sum(c.duration) sum
-    from temp t
-    join calls c
-    on t.id = c.callee_id
-    group by t.country_name
-) t2
- on t1.country_name = t2.country_name
+    join (
+        select t.country_name, count(c2.callee_id) cnt, ifnull(sum(c2.duration), 0) sum
+        from temp t
+        left join calls c2
+        on t.id = c2.callee_id
+        group by t.country_name
+    ) t2
+on t1.country_name = t2.country_name
 where (t1.sum + t2.sum) / (t1.cnt + t2.cnt) > (select avg from avg)
+
