a9b26c566b70ab1573991a517f65a8fd4f45a6ce
LHS <128116509+LeeHusung@users.noreply.github.com>
2026-03-19 09:09:12 +0900

    Time: 0 ms (100%), Space: 43.6 MB (49.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,76 +2,46 @@
 // 2. 
 import java.util.*;
 class Solution {
+    private String phoneDigits;
+    private List<String> answer = new ArrayList<>();
+        private Map<Character, String> letters = Map.of(
+        '2',
+        "abc",
+        '3',
+        "def",
+        '4',
+        "ghi",
+        '5',
+        "jkl",
+        '6',
+        "mno",
+        '7',
+        "pqrs",
+        '8',
+        "tuv",
+        '9',
+        "wxyz"
+    );
+    public void backtracking(int idx, StringBuilder sb) {
+        if (sb.length() == phoneDigits.length()) {
+            answer.add(sb.toString());
+            return;
+        }
+
+        char x = phoneDigits.charAt(idx);
+        String value = letters.get(x);
+        for (char k : value.toCharArray()) {
+            sb.append(k);
+            backtracking(idx + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+     
+    }
+
     public List<String> letterCombinations(String digits) {
-        Map<Character, List<String>> map = new HashMap<>();
+        phoneDigits = digits;
 
-        map.put('2', List.of("a", "b", "c"));
-        map.put('3', List.of("d", "e", "f"));
-        map.put('4', List.of("g", "h", "i"));
-        map.put('5', List.of("j", "k", "l"));
-        map.put('6', List.of("m", "n", "o"));
-        map.put('7', List.of("p", "q", "r", "s"));
-        map.put('8', List.of("t", "u", "v"));
-        map.put('9', List.of("w", "x", "y", "z"));
-
-        List<String> answer = new ArrayList<>();
-        int len = digits.length();
-        if (len == 1) {
-            return map.get(digits.charAt(0));
-        }
-
-        if (len == 2) {
-            char x = digits.charAt(0);
-            char y = digits.charAt(1);
-            List<String> one = map.get(x);
-            List<String> two = map.get(y);
-            for (int i = 0; i < one.size(); i++) {
-                for (int j = 0; j < two.size(); j++) {
-                    answer.add(one.get(i)+two.get(j));
-                }
-            }
-            return answer;
-        }
-
-        if (len == 3) {
-            char x = digits.charAt(0);
-            char y = digits.charAt(1);
-            char z = digits.charAt(2);
-            List<String> one = map.get(x);
-            List<String> two = map.get(y);
-            List<String> three = map.get(z);
-            for (int i = 0; i < one.size(); i++) {
-                for (int j = 0; j < two.size(); j++) {
-                    for (int k = 0; k < three.size(); k++) {
-                        answer.add(one.get(i)+two.get(j)+three.get(k));
-                    }
-                }
-            }
-            return answer;
-        }
-
-        if (len == 4) {
-            char x = digits.charAt(0);
-            char y = digits.charAt(1);
-            char z = digits.charAt(2);
-            char o = digits.charAt(3);
-            List<String> one = map.get(x);
-            List<String> two = map.get(y);
-            List<String> three = map.get(z);
-            List<String> four = map.get(o);
-            for (int i = 0; i < one.size(); i++) {
-                for (int j = 0; j < two.size(); j++) {
-                    for (int k = 0; k < three.size(); k++) {
-                        for (int w = 0; w < four.size(); w++) {
-                            answer.add(one.get(i)+two.get(j)+three.get(k)+four.get(w));
-                        }
-                    }
-                }
-            }
-            return answer;
-        }
-        
-
-        return null;
+        backtracking(0, new StringBuilder());
+        return answer;
     }
 }
