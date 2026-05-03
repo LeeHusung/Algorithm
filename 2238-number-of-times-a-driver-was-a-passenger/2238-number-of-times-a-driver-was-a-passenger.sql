9c3cb04d29bf5d2d604b6b45d16d229bc5e6675b
LeeHusung <128116509+LeeHusung@users.noreply.github.com>
2026-05-03 13:43:22 +0900

    Time: 491 ms (39.46%), Space: 0B (100%) - LeetHub

--- 2238-number-of-times-a-driver-was-a-passenger/2238-number-of-times-a-driver-was-a-passenger.sql
+++ 2238-number-of-times-a-driver-was-a-passenger/2238-number-of-times-a-driver-was-a-passenger.sql
@@ -1,9 +1,5 @@
-select t1.driver_id, count(t2.passenger_id) cnt
-from (
-    select driver_id
-    from rides t1
-    group by driver_id
-    ) t1
+select t1.driver_id, count(distinct t2.ride_id) cnt
+from rides t1
 left join rides t2
  on t1.driver_id = t2.passenger_id
 group by t1.driver_id
