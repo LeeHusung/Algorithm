e1b884304697e4b6903f0dc09dc32289d21ba652
LeeHusung <128116509+LeeHusung@users.noreply.github.com>
2026-05-19 13:59:56 +0900

    Time: 25 ms (60.25%), Space: 86.3 MB (28.01%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -5,34 +5,22 @@ class Solution {
             return 0;
         }
         Arrays.sort(nums);
-        // System.out.println(nums.length);
-        // System.out.println(Arrays.toString(nums));
         int s = nums[0];
         int e = nums[nums.length - 1];
         if (e <= (long) s * k) return 0;
 
         long res = Long.MAX_VALUE;
         
+        int j = 0;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            long limit = (long) nums[i] * k;
-            int start = i + 1;
-            int end = nums.length - 1;
-            while (start <= end) {
-                int middle = (start + end) / 2;
-                if (nums[middle] > limit) {
-                    end = middle - 1;
-                } else {
-                    start = middle + 1;
-                }
+            while ((long) k * nums[j] < nums[i]) {
+                j++;
             }
-            // System.out.println("end: " + end);
-            // System.out.println(i + nums.length - end - 1);
-            res = Math.min(res, i + nums.length - end - 1);
+            max = Math.max(max, i - j + 1);
         }
-        
 
-
-        return (int) res;
+        return nums.length - max;
 
     }
 }
