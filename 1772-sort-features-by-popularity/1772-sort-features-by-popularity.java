b134f277025f3e99cb9ebb358c9ec06926b3733a
LeeHusung <128116509+LeeHusung@users.noreply.github.com>
2026-07-24 07:49:20 +0900

    Time: 54 ms (73.33%), Space: 49.2 MB (100%) - LeetHub

--- 1772-sort-features-by-popularity/1772-sort-features-by-popularity.java
+++ 1772-sort-features-by-popularity/1772-sort-features-by-popularity.java
@@ -2,11 +2,9 @@ import java.util.*;
 class Solution {
     public String[] sortFeatures(String[] features, String[] responses) {
         Map<String, Integer> map = new HashMap<>();
-        Map<String, Integer> count = new HashMap<>();
         int cnt = 0;
         for (String feature : features) {
             map.put(feature, 0);
-            count.put(feature, cnt++);
         }
 
         Set<String> set = new HashSet<>();
@@ -23,24 +21,7 @@ class Solution {
             set.clear();
         }
 
-        List<String> keys = new ArrayList<>(map.keySet());
-        keys.sort(new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                if (map.get(s2) == map.get(s1)) {
-                    return count.get(s1).compareTo(count.get(s2));
-                } else {
-                    return map.get(s2).compareTo(map.get(s1));
-                }
-            }
-        });
-
-        String[] res = new String[features.length];
-        int idx = 0;
-        for (String key : keys) {
-            res[idx++] = key;
-        }
-        return res;
-
+        Arrays.sort(features, (a, b) -> map.get(b) - map.get(a));
+        return features;
     }
 }
